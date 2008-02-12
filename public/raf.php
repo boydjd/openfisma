d61d1665dd6e8dae302ee4799b0cfda6178d8e97
James Ford <jford@endeavorsystems.com>
2008-02-12 02:12:55 +0000

    removed bad language in comments.

--- public/raf.php
+++ public/raf.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * Yes, I know it's crazy. But this is the only simple way to finish our requirements whith a little change.
- * *** Kick the author who did this shit before! ***
- */
 require_once('raf.inc.php');
 $smarty->display('raf.tpl');
 ?>
