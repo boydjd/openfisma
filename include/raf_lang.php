19e0e34cb0109fca6de6b4f9ea0182caeb2ae085
James Ford <jford@endeavorsystems.com>
2008-02-12 02:17:08 +0000

    updated raf.class.php

--- include/raf_lang.php
+++ include/raf_lang.php
@@ -1,40 +1,49 @@
 <?PHP
 $raf_lang= array (
-array ("Risk Analysis Form(RAF): "),
+array ("Risk Analysis Form (RAF)"),
 array (
-	"Weakness/Vulnerability Tracking #:",	"Date Discovered:",
-	"Principle Office:", "System:",	"Date Opened:",
-	"Weakness/Vulnerability Description:","Date Modified:",
-	"Source that identified the Weakness/Vulnerability:","Date Closed:",
-	"Is this a repeat finding from a previous OIG or GAO Audit?","List the audit(s):"
+	"Finding Tracking #:",	
+        "Date Discovered:",
+	"Principle Office:", 
+        "System:",	
+        "Date Opened:",
+	"Finding Description:",
+        "Date Modified:",
+	"Finding Source:",
+        "Date Closed:",
+	"Is this a repeat finding?",
+        "List the audit(s):"
 	),
-array("Mission Criticality of affected subsystem or system:",
-	"Justification for rating:",
-	"Data Sensitivity impact level of affected subsystem of system",
-	"Justification for rating:",
+
+array(
+	"Mission Criticality:",
+	"Justification:",
+	"Data Sensitivity Level:",
+	"Justification:",
 	"Overall Impact Rating:"
 	),
-	array(
-		"Specific Countermeasure Applied:",
-		"Countermeasure Effectiveness:",
-		"Justification for rating:",
-		"Threat Source:",
-		"List of specific source:",
-		"Justification for rating:",
-		"Overall Likelihood Rating (From Likelihood Table):",
-		"Affected Servers:",
-	),
-	array(
-	"Overall Weakness Risk Level:",
+
+array(
+	"Countermeasures:",
+	"Countermeasure Effectiveness:",
+	"Justification for Rating:",
+	"Threat Source:",
+	"List of specific source:",
+	"Justification for Rating:",
+	"Overall Likelihood Rating:",
+	"Affected Servers:",
+),
+array(
+	"Overall Risk Level:",
 	"High:","Strong need for corrective actions as soon as possible (provide POA&M)",
 	"Moderate:","Need for corrective action within a reasonable time period (Provide POA&M)",
 	"Low:","Authorizing Official may correct or accept the risk (provide POA&M which may include justification for accepting the risk)",
-	),
-	array(
+),
+array(
 	"Recommendation(s):",
 	"Course of Action:",
-	),
-	array(
+),
+array(
 	"WARNING: This report is for internal, official use only.  This report contains sensitive computer security related information. Public disclosure of this information would risk circumvention of the law. Recipients of this report must not, under any circumstances, show or release its contents for purposes other than official action. This report must be safeguarded to prevent improper disclosure. Staff reviewing this document must hold a minimum of Public Trust Level 5C")
 	
 	
