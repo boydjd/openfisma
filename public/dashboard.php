821325c6d835274f2af59ca7e7b1786cbb97c8d9
xhorse <unknown@reyosoft.com>
2008-02-15 06:20:19 +0000

    fix the bug in dashboard in condition that all data are zero

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,4 +1,5 @@
 <?PHP
+/* vim: set tabstop=4 shiftwidth=4 expandtab: */
 header("Cache-Control: no-cache, must-revalidate"); 
 
 // include files
@@ -32,7 +33,7 @@ $Role_ID = $user->getRoleId() ;
 verify_login($user, $smarty);
 
 // get user right for this screen
-$view_right	= $user->checkRightByFunction($screen_name, "view");
+$view_right = $user->checkRightByFunction($screen_name, "view");
 
 // let's template know how to display the page
 $smarty->assign('view_right', $view_right);
@@ -41,36 +42,56 @@ $smarty->assign('view_right', $view_right);
 if($view_right) 
 {
 
-  // conditionally require dashboard items
-  require_once("dashboard_chart/Dashboard_Chart.php");
-  require_once("dashboard_chart/create_xml.php");
-  require_once("PoamSummary.class.php");
+    // conditionally require dashboard items
+    require_once("dashboard_chart/Dashboard_Chart.php");
+    require_once("dashboard_chart/create_xml.php");
+    require_once("PoamSummary.class.php");
 
-  // Get chart xml data and create charts
-  $summary = generate_summary($user, new PoamSummary($db));
-  create_xml_1($summary['total_open'], $summary['total_en'], $summary['total_eo'], $summary['total_ep'], $summary['total_es'], $summary['total_closed']);
-  create_xml_2($summary['total_open'], $summary['total_en'], $summary['total_eo'], $summary['total_ep'], $summary['total_es'], $summary['total_closed']);
-  create_xml_3($summary['total_none'], $summary['total_cap'], $summary['total_fp'], $summary['total_ar']);
-  //create_xml_3(43, 31, 41, 55);
+    // Get chart xml data and create charts
+    $summary = generate_summary($user, new PoamSummary($db));
 
-  // create a new chart and insert charts
-  $dbc1 = new Dashboard_Chart;	
-  $smarty->assign('dashboard1', $dbc1->InsertChart("temp/dashboard1.xml", "380" , "220"));
-  $smarty->assign('dashboard2', $dbc1->InsertChart("temp/dashboard2.xml" , "200" , "220"));
-  $smarty->assign('dashboard3', $dbc1->InsertChart("temp/dashboard3.xml" , "380" , "220"));
-	
+    $sumSig = $summary['total_none']+$summary['total_cap']+$summary['total_fp']+$summary['total_ar'];
+    if($sumSig){
+
+        create_xml_1($summary['total_open'],
+                     $summary['total_en'], 
+                     $summary['total_eo'], 
+                     $summary['total_ep'],
+                     $summary['total_es'],
+                     $summary['total_closed']);
+
+        create_xml_2($summary['total_open'],
+                     $summary['total_en'],
+                     $summary['total_eo'], 
+                     $summary['total_ep'],
+                     $summary['total_es'], 
+                     $summary['total_closed']);
+
+        create_xml_3($summary['total_none'], 
+                     $summary['total_cap'], 
+                     $summary['total_fp'], 
+                     $summary['total_ar']);
+    
+        //create_xml_3(43, 31, 41, 55);
+
+        // create a new chart and insert charts
+        $dbc1 = new Dashboard_Chart;    
+        $smarty->assign('dashboard1', $dbc1->InsertChart("temp/dashboard1.xml", "380" , "220"));
+        $smarty->assign('dashboard2', $dbc1->InsertChart("temp/dashboard2.xml" , "200" , "220"));
+        $smarty->assign('dashboard3', $dbc1->InsertChart("temp/dashboard3.xml" , "380" , "220"));
+    }
 
   // 
   // ALERTS
   // 
 
-  //$smarty->assign('need_type',  '<li>There are <b>' . $summary['total_none'] . '</b> finding(s) awaiting clasification.');
-  $smarty->assign('need_mit',   '<li>There are <b>' . $summary['total_open'] . '</b> finding(s) awaiting a mitigation strategy and approval.');
-  $smarty->assign('need_ev_ot', '<li>There are <b>' . $summary['total_en'] .   '</b> finding(s) awaiting evidence.');
-  $smarty->assign('need_ev_od', '<li>There are <b>' . $summary['total_eo'] .   '</b> overdue finding(s) awaiting evidence.');
+    //$smarty->assign('need_type',  '<li>There are <b>' . $summary['total_none'] . '</b> finding(s) awaiting clasification.');
+    $smarty->assign('need_mit',   '<li>There are <b>' . $summary['total_open'] . '</b> finding(s) awaiting a mitigation strategy and approval.');
+    $smarty->assign('need_ev_ot', '<li>There are <b>' . $summary['total_en'] .   '</b> finding(s) awaiting evidence.');
+    $smarty->assign('need_ev_od', '<li>There are <b>' . $summary['total_eo'] .   '</b> overdue finding(s) awaiting evidence.');
 
-}	
+}   
 
-// display the following template	
+// display the following template   
 $smarty->display('dashboard.tpl');
 ?>
