746d44eb07735699ac4754fc2d1f7f4a5b6c2a1b
James Ford <jford@endeavorsystems.com>
2008-02-18 17:46:56 +0000

    classload.php added back to the repository as it is required by the install script.

---
+++ include/classload.php
@@ -0,0 +1,13 @@
+<?php
+/** This is an autoloading mechanism. 
+ *  All the classes in a single file should follow the naming convention
+ */
+ // required by the install script in /public/install
+function __autoload($class_name) {
+    if( @!include_once(dirname(__FILE__) . "/class/$class_name.class.php") ){
+        eval('class '.$class_name.' extends Exception {}');
+        throw new $class_name("[autoloading]:Class $class_name is not defined");
+    }
+}
+
+?>
