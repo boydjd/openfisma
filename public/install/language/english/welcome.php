f48f8f9c4ba2623b77fdac83c78ae1a4fe2c7358
James Ford <jford@endeavorsystems.com>
2008-02-29 01:47:16 +0000

    updated welcome.php installation language

--- public/install/language/english/welcome.php
+++ public/install/language/english/welcome.php
@@ -1,6 +1,6 @@
 <?php
 $content .= '
-<P>OpenFISMA is an open source application designed to reduce the complexity and automate the regulatory requirements of the Federal Information Security Management Act (FISMA) and the National Institute of Science and Technology (NIST) Risk Management Framework (RMF). While many security managers are eager to demonstrate their best practices for incident response, patch management, and configuration management, they are overwhelmed with the reporting and documentation requirements of FISMA. Below is a brief listing of some of the documents that OpenFISMA automates:</p>
+OpenFISMA is an open source application designed to reduce the complexity and automate the regulatory requirements of the Federal Information Security Management Act (FISMA) and the National Institute of Science and Technology (NIST) Risk Management Framework (RMF). While many security managers are eager to demonstrate their best practices for incident response, patch management, and configuration management, they are overwhelmed with the reporting and documentation requirements of FISMA. Below is a brief listing of some of the documents that OpenFISMA automates:
 <UL>
 <LI> System Security Plan 
 <LI> Privacy Impact Assessment 
@@ -10,20 +10,18 @@ $content .= '
 <LI> Plan of Action and Milestones (POAM) 
 </UL>
 
-<p>
-<b>SOFTWARE REQUIREMENTS</b>
-
+<b>SOFTWARE REQUIREMENTS</b><br>
+<br>
 OpenFISMA requires the following software, all of the following software should be installed prior to installing OpenFISMA. The installation program will check to ensure you have the correct versions of the software required installed on your system.
 <UL>
 <LI> Apache version 2+
 <LI> PHP version 5+
 <LI> MySQL version 5+ 
-<LI> Smarty version 2.6.18+  (Optional)
-<LI> Adodblite (Optional)
-<LI> Pear OLE and Spreadsheet_Excel_Writer (Optional)
+<LI> Perl
 </UL>
 
-<b>ADVANCED INSTALLATION INSTRUCTIONS</b>
+<b>ADVANCED INSTALLATION INSTRUCTIONS</b><br>
+<br>
 Refer to INSTALL file under the distributed directory.
-</P>';
+';
 ?>
