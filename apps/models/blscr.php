6718f88d871482cb8412b2c68983d001e9a2df05
Mark E. Haase <mhaase@endeavorsystems.com>
2008-07-18 21:03:04 +0000

    Make the default sort on BLSRC to be ascending sort on the BLSCR code (e.g. CP-03).

--- apps/models/blscr.php
+++ apps/models/blscr.php
@@ -2,12 +2,15 @@
 /**
  * @file blscr.php
  *
- * @description blscr model
+ * BLSCR means baseline security control requirements. NIST 800-53 specifies
+ * which security controls apply to each level of system: LOW, MODERATE, and HIGH.
+ * The baseline requirments specify a greater level of security for systems
+ * with a higher impact rating.
  *
  * @author     Jim <jimc@reyosoft.com>
  * @copyright  (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
  * @license    http://www.openfisma.org/mw/index.php?title=License
- * @version $Id$
+ * @version    $Id$
 */
 
 require_once 'Abstract.php';
@@ -15,6 +18,30 @@ class Blscr extends Fisma_Model
 {
     protected $_name = 'blscrs';
     protected $_primary = 'code';
+    
+    /**
+     * getList
+     *
+     * Overrides the parent function in order to enforce sorting on the `code`
+     * column, which in most cases will be the most intuitive sort for the end
+     * user.
+     *
+     * If the caller specifies a non-null sort order, then the sort order is
+     * not changed.
+     *
+     * @param string|string[] $fields Which field[s] to get from the BLSCR list
+     * @param string $primary_key Which column becomes the key for the returned array (defaults to the primary key of the table)
+     * @param string|string[] $order The column[s] to sort on (defaults to `code`)
+     *
+     * @return array An array of arrays where the primary_key is the outer key and the column names are the inner keys
+     */
+    public function getList($fields = '*', $primary_key = null, $order=null) {
+        if ($order == null) {
+            return parent::getList($fields, $primary_key, 'code');
+        } else {
+            return parent::getList($fields, $primary_key, $order);
+        }
+    }
 }
 
 ?>
