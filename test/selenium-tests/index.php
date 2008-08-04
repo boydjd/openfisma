dc303c07d17fef01b4442975312d543944ae1858
James Ford <jford@endeavorsystems.com>
2008-08-04 21:13:15 +0000

    testing id substitution using tortoise svn

--- test/selenium-tests/index.php
+++ test/selenium-tests/index.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * index.php
- * @package  Test_Selenium
- * @author     Xhorse   xhorse at users.sourceforge.net
- * @copyright  (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
- * @license    http://www.openfisma.org/mw/index.php?title=License
- * $Id$
+ * @package  	Test_Selenium
+ * @author     	Xhorse   xhorse at users.sourceforge.net
+ * @copyright  	(c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license    	http://www.openfisma.org/mw/index.php?title=License
+ * @version		$Id$
 */
 
 header('Location:/test');
