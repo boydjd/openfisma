aee502414c796822b77d4fc984ddcd1c90b81c51
xhorse <unknown@reyosoft.com>
2008-10-14 15:33:30 +0000

    FIX:checkbox cannot be changed error. The reason is that checkboxs value should be the checkedValue instead of the variable from database

--- apps/Form/FismaDecorator.php
+++ apps/Form/FismaDecorator.php
@@ -67,9 +67,14 @@ class Form_FismaDecorator extends Zend_Form_Decorator_Abstract
     public function buildInput() {
         $element = $this->getElement();
         $helper  = $element->helper;
+        if ($element instanceof Zend_Form_Element_Checkbox) {
+            $value = $element->getCheckedValue();
+        }else{
+            $value = $element->getValue();
+        }
         return $element->getView()->$helper(
             $element->getName(),
-            $element->getValue(),
+            $value,
             $element->getAttribs(),
             $element->options
         );
