74d1791d6a79369ad6d4c57f1edcc02f6843b714
James Ford <jford@endeavorsystems.com>
2008-10-15 15:55:37 +0000

    Adding demo_data.sql file which will inject demonstration data for the demo server and can be used to load test data

---
+++ migrations/demo_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `networks` (id, name, nickname, `desc`) VALUES (1,'Test Data Center A','TDCA','');
+INSERT INTO `sources` (id, name, nickname, `desc`) VALUES (1,'Database Scanner','DB','');
+INSERT INTO `systems` (id, name, nickname, `desc`, `type`, confidentiality, integrity, availability) VALUES (1,'Test System A','TSA','','GENERAL SUPPORT SYSTEM','HIGH','HIGH','HIGH'),(2,'Test System B','TSB','','GENERAL SUPPORT SYSTEM','HIGH','HIGH','HIGH');
