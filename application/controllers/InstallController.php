6fe0eb2bd4f3caac91e2fe3359e5eadcad0ab4ea
Ryan Yang <ryan.yang@reyosoft.com>
2008-10-21 09:39:43 +0000

    correct the log dirctory name

--- application/controllers/InstallController.php
+++ application/controllers/InstallController.php
@@ -65,7 +65,7 @@ class InstallController extends Zend_Controller_Action
     {
         $wDirectories = array(
             WEB_ROOT . '/temp',
-            ROOT . '/data/log',
+            ROOT . '/data/logs',
             WEB_ROOT . '/evidence',
             CONFIGS .'/'. Config_Fisma::CONFIGFILE_NAME
         );
