9deb545348f595c74a01bd0e7672a3a2dddb2df2
Ryan Yang <ryan.yang@reyosoft.com>
2008-11-03 08:31:12 +0000

    delete undefined "LIBS", add "PDF" for generate pdf files

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -44,6 +44,9 @@ defined('APPLICATION_LOGS')
 defined('LOCAL')
     or define('LOCAL', APPLICATION_ROOT . '/library/local');
 
+defined('PDF')
+    or define('PDF', APPLICATION_ROOT . '/library/pdf');
+
 defined('PEAR')
     or define('PEAR', APPLICATION_ROOT . '/library/Pear');
 
