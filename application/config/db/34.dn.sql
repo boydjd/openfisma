46526a2d18f7bf32b8d519eeb15b20283d9d759b
Mark E. Haase <mhaase@endeavorsystems.com>
2008-11-17 17:02:09 +0000

    I'm modifying these patches. Ordinarily we shouldn't do this, but this is an emergency since this will fail the upgrade process for OVMS and will reset the root password if executed in production.
    I'm not updating the base.sql; that file is still correct with respect to the root password.

--- application/config/db/34.dn.sql
+++ application/config/db/34.dn.sql
@@ -5,9 +5,3 @@
 -- Version:   $Id:$
 --
 
--- Add a function for injecting findings. This is distinct from the function for creating findings.
-
---UPDATE `users` SET `created_ts` = NOW() AND `password_ts` = NOW() WHERE `id` = '1';
-DELETE FROM `users` WHERE `id` = '1';
-INSERT INTO `users` VALUES (1,'root','4a95bac3e19b28ee0acf3cc1137b4d1e66720a49','admin','Application',NULL,'Admin','0000-00-00 00:00:00','0000-00-00 00:00:00','','0000-00-00 00:00:00','','0000-00-00 00:00:00',1,0,'',NULL,'',0,'root_r',720,'0000-00-00 00:00:00','','','0000-00-00 00:00:00');
-
