32a7ee793b5e8283eb6993178f638171b48c4111
Ryan Yang <ryan.yang@reyosoft.com>
2008-11-21 11:58:45 +0000

    correct the 40th upgrade sql

---
+++ application/config/db/41.dn.sql
@@ -0,0 +1,8 @@
+--
+-- Author:    Ryan yang<ryan.yang@reyosoft.com>
+-- Copyright: (c) 2008 Endeavor Systems, Inc.
+-- License:   http://www.openfisma.org/mw/index.php?title=License
+-- Version:   $Id:$
+--
+
+UPDATE `users` SET `hash`= 'sha1' WHERE `id` = 1;
