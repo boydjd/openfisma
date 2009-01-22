7c4bb62ca5c0ef70a2a86d4465a832ec340f0fba
Mark E. Haase <mhaase@endeavorsystems.com>
2009-01-22 19:53:04 +0000

    #2529181  	 RAF - threat likelihood calculation is incorrect
    Modify the logic to use a lookup table instead. Previously we were trying to reduce dependencies on constants and get this information directly from the DBMS, but that code was really complicated to make it work. For constants and calculations that are defined in federal standards (this one is defined in NIST SP 800-30) we should put the constants and calculations directly into our code in order to make the code more readable.
    
    I'm also including a unit test for this fix.

---
+++ tests/FismaUnitTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author    Mark E. Haase <mhaase@endeavorsystems.com>
+ * @copyright (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license   http://www.openfisma.org/mw/index.php?title=License
+ * @version   $Id$
+ */
+
+// Run the application bootstrap in command line mode
+if (!defined('COMMAND_LINE')) {
+    define('COMMAND_LINE', true);
+    require_once(realpath(dirname(__FILE__)."/../application/bootstrap.php"));
+}
+
+// Load the base class
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * This is the base class for all unit tests in OpenFISMA. Currently this class is just a stub. The real reason for
+ * including it is to execute the application bootstrap logic contained in this same file.
+ *
+ * @package Test
+ */
+abstract class Test_FismaUnitTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Set up access to the database.
+     *
+     * @todo why isn't this done in the bootstrap?
+     */
+    protected function setUp()
+    {
+        // Initialize our DB connection
+        $this->_db = Zend_Db::factory(Zend_Registry::get('datasource'));
+        Zend_Db_Table::setDefaultAdapter($this->_db);
+    }
+}
