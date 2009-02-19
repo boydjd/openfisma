38cd59175e564aaa87b71d0ec31053f454170440
Mark E. Haase <mhaase@endeavorsystems.com>
2009-02-19 20:35:11 +0000

    Bug fix on the trunk: 2604556  	 db migration is broken.
    Breaking the policy to fix this. The 49.up.sql file had subversion conflict markers in it that I should have removed when I did the previous week's merge. I had to modify that file and then update the version in base.sql manually.

--- application/config/db/base.sql
+++ application/config/db/base.sql
@@ -1009,4 +1009,4 @@ CREATE TABLE `vulnerabilities` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 TRUNCATE TABLE schema_version;
-INSERT INTO schema_version (schema_version) VALUES (49);
+INSERT INTO schema_version (schema_version) VALUES (50);
