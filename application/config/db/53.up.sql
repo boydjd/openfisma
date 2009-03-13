18c2bb26cfebf0a387961408b4acd4b83ee6e77b
Ryan Yang <ryan.yang@reyosoft.com>
2009-03-13 05:29:48 +0000

    Feature request on trunk #2614273 Update blscrs table to NIST 800-53 Revision 2
    Correct the primary keys, such as AC-1 is changed into AC-01

---
+++ application/config/db/53.up.sql
@@ -0,0 +1,151 @@
+--
+-- Author:    Ryan yang <ryanyang@reyosoft.com>
+-- Copyright: (c) 2008 Endeavor Systems, Inc.
+-- License:   http://www.openfisma.org/mw/index.php?title=License
+-- Version:   $Id$
+--
+
+UPDATE blscrs SET code = 'AC-01' WHERE code = 'AC-1 ';
+UPDATE blscrs SET code = 'AC-02' WHERE code = 'AC-2 ';
+UPDATE blscrs SET code = 'AC-03' WHERE code = 'AC-3 ';
+UPDATE blscrs SET code = 'AC-04' WHERE code = 'AC-4 ';
+UPDATE blscrs SET code = 'AC-05' WHERE code = 'AC-5 ';
+UPDATE blscrs SET code = 'AC-06' WHERE code = 'AC-6 ';
+UPDATE blscrs SET code = 'AC-07' WHERE code = 'AC-7 ';
+UPDATE blscrs SET code = 'AC-08' WHERE code = 'AC-8 ';
+UPDATE blscrs SET code = 'AC-09' WHERE code = 'AC-9 ';
+
+UPDATE blscrs SET code = 'AT-01' WHERE code = 'AT-1 ';
+UPDATE blscrs SET code = 'AT-02' WHERE code = 'AT-2 ';
+UPDATE blscrs SET code = 'AT-03' WHERE code = 'AT-3 ';
+UPDATE blscrs SET code = 'AT-04' WHERE code = 'AT-4 ';
+UPDATE blscrs SET code = 'AT-05' WHERE code = 'AT-5 ';
+
+UPDATE blscrs SET code = 'AU-01' WHERE code = 'AU-1 ';
+UPDATE blscrs SET code = 'AU-02' WHERE code = 'AU-2 ';
+UPDATE blscrs SET code = 'AU-03' WHERE code = 'AU-3 ';
+UPDATE blscrs SET code = 'AU-04' WHERE code = 'AU-4 ';
+UPDATE blscrs SET code = 'AU-05' WHERE code = 'AU-5 ';
+UPDATE blscrs SET code = 'AU-06' WHERE code = 'AU-6 ';
+UPDATE blscrs SET code = 'AU-07' WHERE code = 'AU-7 ';
+UPDATE blscrs SET code = 'AU-08' WHERE code = 'AU-8 ';
+UPDATE blscrs SET code = 'AU-09' WHERE code = 'AU-9 ';
+
+UPDATE blscrs SET code = 'CA-01' WHERE code = 'CA-1';
+UPDATE blscrs SET code = 'CA-02' WHERE code = 'CA-2 ';
+UPDATE blscrs SET code = 'CA-03' WHERE code = 'CA-3 ';
+UPDATE blscrs SET code = 'CA-04' WHERE code = 'CA-4 ';
+UPDATE blscrs SET code = 'CA-05' WHERE code = 'CA-5 ';
+UPDATE blscrs SET code = 'CA-06' WHERE code = 'CA-6 ';
+UPDATE blscrs SET code = 'CA-07' WHERE code = 'CA-7 ';
+
+UPDATE blscrs SET code = 'CM-01' WHERE code = 'CM-1 ';
+UPDATE blscrs SET code = 'CM-02' WHERE code = 'CM-2 ';
+UPDATE blscrs SET code = 'CM-03' WHERE code = 'CM-3 ';
+UPDATE blscrs SET code = 'CM-04' WHERE code = 'CM-4 ';
+UPDATE blscrs SET code = 'CM-05' WHERE code = 'CM-5 ';
+UPDATE blscrs SET code = 'CM-06' WHERE code = 'CM-6 ';
+UPDATE blscrs SET code = 'CM-07' WHERE code = 'CM-7 ';
+UPDATE blscrs SET code = 'CM-08' WHERE code = 'CM-8 ';
+
+UPDATE blscrs SET code = 'CP-01' WHERE code = 'CP-1 ';
+UPDATE blscrs SET code = 'CP-02' WHERE code = 'CP-2 ';
+UPDATE blscrs SET code = 'CP-03' WHERE code = 'CP-3 ';
+UPDATE blscrs SET code = 'CP-04' WHERE code = 'CP-4 ';
+UPDATE blscrs SET code = 'CP-05' WHERE code = 'CP-5 ';
+UPDATE blscrs SET code = 'CP-06' WHERE code = 'CP-6 ';
+UPDATE blscrs SET code = 'CP-07' WHERE code = 'CP-7 ';
+UPDATE blscrs SET code = 'CP-08' WHERE code = 'CP-8 ';
+UPDATE blscrs SET code = 'CP-09' WHERE code = 'CP-9 ';
+
+UPDATE blscrs SET code = 'IA-01' WHERE code = 'IA-1';
+UPDATE blscrs SET code = 'IA-02' WHERE code = 'IA-2 ';
+UPDATE blscrs SET code = 'IA-03' WHERE code = 'IA-3 ';
+UPDATE blscrs SET code = 'IA-04' WHERE code = 'IA-4 ';
+UPDATE blscrs SET code = 'IA-05' WHERE code = 'IA-5 ';
+UPDATE blscrs SET code = 'IA-06' WHERE code = 'IA-6 ';
+UPDATE blscrs SET code = 'IA-07' WHERE code = 'IA-7 ';
+
+UPDATE blscrs SET code = 'IR-01' WHERE code = 'IR-1 ';
+UPDATE blscrs SET code = 'IR-02' WHERE code = 'IR-2 ';
+UPDATE blscrs SET code = 'IR-03' WHERE code = 'IR-3 ';
+UPDATE blscrs SET code = 'IR-04' WHERE code = 'IR-4 ';
+UPDATE blscrs SET code = 'IR-05' WHERE code = 'IR-5 ';
+UPDATE blscrs SET code = 'IR-06' WHERE code = 'IR-6 ';
+UPDATE blscrs SET code = 'IR-07' WHERE code = 'IR-7 ';
+
+UPDATE blscrs SET code = 'MA-01' WHERE code = 'MA-1 ';
+UPDATE blscrs SET code = 'MA-02' WHERE code = 'MA-2 ';
+UPDATE blscrs SET code = 'MA-03' WHERE code = 'MA-3 ';
+UPDATE blscrs SET code = 'MA-04' WHERE code = 'MA-4 ';
+UPDATE blscrs SET code = 'MA-05' WHERE code = 'MA-5 ';
+UPDATE blscrs SET code = 'MA-06' WHERE code = 'MA-6 ';
+ 
+UPDATE blscrs SET code = 'MP-01' WHERE code = 'MP-1 ';
+UPDATE blscrs SET code = 'MP-02' WHERE code = 'MP-2 ';
+UPDATE blscrs SET code = 'MP-03' WHERE code = 'MP-3 ';
+UPDATE blscrs SET code = 'MP-04' WHERE code = 'MP-4 ';
+UPDATE blscrs SET code = 'MP-05' WHERE code = 'MP-5 ';
+UPDATE blscrs SET code = 'MP-06' WHERE code = 'MP-6 ';
+
+UPDATE blscrs SET code = 'PE-01' WHERE code = 'PE-1';
+UPDATE blscrs SET code = 'PE-02' WHERE code = 'PE-2 ';
+UPDATE blscrs SET code = 'PE-03' WHERE code = 'PE-3 ';
+UPDATE blscrs SET code = 'PE-04' WHERE code = 'PE-4 ';
+UPDATE blscrs SET code = 'PE-05' WHERE code = 'PE-5 ';
+UPDATE blscrs SET code = 'PE-06' WHERE code = 'PE-6 ';
+UPDATE blscrs SET code = 'PE-07' WHERE code = 'PE-7 ';
+UPDATE blscrs SET code = 'PE-08' WHERE code = 'PE-8 ';
+UPDATE blscrs SET code = 'PE-09' WHERE code = 'PE-9 ';
+
+UPDATE blscrs SET code = 'PL-01' WHERE code = 'PL-1 ';
+UPDATE blscrs SET code = 'PL-02' WHERE code = 'PL-2 ';
+UPDATE blscrs SET code = 'PL-03' WHERE code = 'PL-3 ';
+UPDATE blscrs SET code = 'PL-04' WHERE code = 'PL-4 ';
+UPDATE blscrs SET code = 'PL-05' WHERE code = 'PL-5 ';
+UPDATE blscrs SET code = 'PL-06' WHERE code = 'PL-6 ';
+
+UPDATE blscrs SET code = 'PS-01' WHERE code = 'PS-1 ';
+UPDATE blscrs SET code = 'PS-02' WHERE code = 'PS-2 ';
+UPDATE blscrs SET code = 'PS-03' WHERE code = 'PS-3 ';
+UPDATE blscrs SET code = 'PS-04' WHERE code = 'PS-4 ';
+UPDATE blscrs SET code = 'PS-05' WHERE code = 'PS-5 ';
+UPDATE blscrs SET code = 'PS-06' WHERE code = 'PS-6 ';
+UPDATE blscrs SET code = 'PS-07' WHERE code = 'PS-7 ';
+UPDATE blscrs SET code = 'PS-08' WHERE code = 'PS-8 ';
+
+UPDATE blscrs SET code = 'RA-01' WHERE code = 'RA-1 ';
+UPDATE blscrs SET code = 'RA-02' WHERE code = 'RA-2 ';
+UPDATE blscrs SET code = 'RA-03' WHERE code = 'RA-3 ';
+UPDATE blscrs SET code = 'RA-04' WHERE code = 'RA-4 ';
+UPDATE blscrs SET code = 'RA-05' WHERE code = 'RA-5 ';
+
+UPDATE blscrs SET code = 'SA-01' WHERE code = 'SA-1';
+UPDATE blscrs SET code = 'SA-02' WHERE code = 'SA-2 ';
+UPDATE blscrs SET code = 'SA-03' WHERE code = 'SA-3 ';
+UPDATE blscrs SET code = 'SA-04' WHERE code = 'SA-4 ';
+UPDATE blscrs SET code = 'SA-05' WHERE code = 'SA-5 ';
+UPDATE blscrs SET code = 'SA-06' WHERE code = 'SA-6 ';
+UPDATE blscrs SET code = 'SA-07' WHERE code = 'SA-7 ';
+UPDATE blscrs SET code = 'SA-08' WHERE code = 'SA-8 ';
+UPDATE blscrs SET code = 'SA-09' WHERE code = 'SA-9 ';
+
+UPDATE blscrs SET code = 'SC-01' WHERE code = 'SC-1';
+UPDATE blscrs SET code = 'SC-02' WHERE code = 'SC-2 ';
+UPDATE blscrs SET code = 'SC-03' WHERE code = 'SC-3 ';
+UPDATE blscrs SET code = 'SC-04' WHERE code = 'SC-4 ';
+UPDATE blscrs SET code = 'SC-05' WHERE code = 'SC-5 ';
+UPDATE blscrs SET code = 'SC-06' WHERE code = 'SC-6 ';
+UPDATE blscrs SET code = 'SC-07' WHERE code = 'SC-7 ';
+UPDATE blscrs SET code = 'SC-08' WHERE code = 'SC-8 ';
+UPDATE blscrs SET code = 'SC-09' WHERE code = 'SC-9 ';
+
+UPDATE blscrs SET code = 'SI-01' WHERE code = 'SI-1 ';
+UPDATE blscrs SET code = 'SI-02' WHERE code = 'SI-2 ';
+UPDATE blscrs SET code = 'SI-03' WHERE code = 'SI-3 ';
+UPDATE blscrs SET code = 'SI-04' WHERE code = 'SI-4 ';
+UPDATE blscrs SET code = 'SI-05' WHERE code = 'SI-5 ';
+UPDATE blscrs SET code = 'SI-06' WHERE code = 'SI-6 ';
+UPDATE blscrs SET code = 'SI-07' WHERE code = 'SI-7 ';
+UPDATE blscrs SET code = 'SI-08' WHERE code = 'SI-8 ';
+UPDATE blscrs SET code = 'SI-09' WHERE code = 'SI-9 ';
