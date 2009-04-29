2bcaeaedaa2eb6575b0802c5f1923932d82f17eb
Ryan Yang <ryan.yang@reyosoft.com>
2009-04-29 03:46:01 +0000

    bug fix on branches #2777892 Edit User: "Generate Password" button does not work
    Changed the generate password button into Yui Button

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -1,17 +1,15 @@
-YAHOO.util.Event.onDOMReady(function () {
+function GeneratePassword () {
     var generatePasswordButton = document.getElementById('generate_password');
-    generatePasswordButton.onclick = function () {
-        YAHOO.util.Connect.asyncRequest('GET',
-                                        '/account/generatepassword/format/html',
-                                        {
-                                            success: function(o) {
-                                                document.getElementById('password').value = o.responseText;
-                                                document.getElementById('confirmPassword').value = o.responseText;
-                                            },
-                                            failure: function(o) {alert('Failed to generate password: ' + o.statusText);}
+    YAHOO.util.Connect.asyncRequest('GET',
+                                    '/account/generatepassword/format/html',
+                                    {
+                                        success: function(o) {
+                                            document.getElementById('password').value = o.responseText;
+                                            document.getElementById('confirmPassword').value = o.responseText;
                                         },
-                                        null);
-        return false;
-    };
-});
+                                        failure: function(o) {alert('Failed to generate password: ' + o.statusText);}
+                                    },
+                                    null);
+    return false;
+}
 
