5326196973518adc7538b8a4b0b316005d9b2552
Ryan Yang <ryan.yang@reyosoft.com>
2009-06-03 05:44:28 +0000

    feature request on branches #2795870 Doctrine Integration
    Create the Doctrine Migrations script, Please config it and just type "php migrate.php" on the command line to implement migrations

---
+++ scripts/bin/migrate.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenFISMA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Ryan yang<ryan.yang@reyosoft.com>
+ * @copyright (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license   http://www.openfisma.org/mw/index.php?title=License
+ * @version   $Id:$
+ * @package   
+ */
+
+/**
+ * This script migrates the database schema from its existing version to the
+ * version designated in the param.
+ *
+ * @package
+ * @copyright (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license   http://www.openfisma.org/mw/index.php?title=License
+ */
+require_once('../../application/init.php');
+//in order to export we need a database connection
+$dbUser     = '';
+$dbPassword = '';
+$dbHost     = 'localhost';
+$dbName     = '';
+
+$migrationDir = '../../application/doctrine/migrations/';
+
+Doctrine_Manager::connection("mysql://$dbUser:$dbPassword@$dbHost/$dbName");
+$migration = new Doctrine_Migration($migrationDir);
+
+//Migrate to the latest version, also ,you can type "$migration->migrate(1)" to the version 1
+$migration->migrate(); 
+
+print('Migration successful.Current version is ' . $migration->getCurrentVersion() . '\n'); 
