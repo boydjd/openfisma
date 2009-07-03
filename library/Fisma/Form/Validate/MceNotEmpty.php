f85f222543ca9942b5798953b1fa3f85dbd0e879
xhorse <unknown@reyosoft.com>
2009-07-03 10:01:11 +0000

    Feature request on branch Doctrine #2795870 Doctrine Integration
    Fix the mce NotEmpty validation issue.

---
+++ library/Fisma/Form/Validate/MceNotEmpty.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenFISMA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    xhorse <xhorse@users.sourceforge.net>
+ * @copyright (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license   http://www.openfisma.org/mw/index.php?title=License
+ * @version   $Id$
+ * @package   Form
+ */
+
+/**
+ * Validate the MCE editor has content
+ *
+ * @package   Form
+ * @copyright (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license   http://www.openfisma.org/mw/index.php?title=License
+ */
+class Fisma_Form_Validate_MceNotEmpty extends Zend_Validate_NotEmpty
+{
+    /** 
+     * Returns true if the mce editor has none empty value after removing the wrapper tags
+     *
+     * @param string $value
+     * @return true|false
+     */
+    public function isValid($value)
+    {
+        $value = preg_replace("/\<p\>( *\<[^>]*\>)*(.*)\<\/p\>/", "$2",  $value); 
+        return parent::isValid($value);
+    }
+}
