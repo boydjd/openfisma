f25c629639102c8c95955c60d67bc4ddc00169e9
Ryan Yang <ryan.yang@reyosoft.com>
2009-07-08 10:39:38 +0000

    Feature request on Trunk #2795870 Doctrine Integration
    Improve the migration to Doctrine script
    2. fix a variable name spelling error

--- scripts/bin/migrateToDoctrine.php
+++ scripts/bin/migrateToDoctrine.php
@@ -44,6 +44,10 @@ class Migrate
         Fisma::setNotificationEnabled(false);
         Fisma::setListenerEnabled(false);    
 
+        if (!file_exists(Fisma::getPath('config') . '/install.conf')) {
+            print Fisma::getPath('config') . "/install.conf is not exist \n";
+            exit;
+        }
         // Load the old database configuration
         $installConfFile = Fisma::getPath('config') . '/install.conf';
         $conf = new Zend_Config_Ini($installConfFile);
@@ -59,7 +63,7 @@ class Migrate
     {
         $table = new $tableName;
         foreach ($data as $field=>$value) {
-            if (!(in_array($field, $table->getTable()->getFieldNames())) || empty($value)) {
+            if (!(in_array($field, $table->getTable()->getFieldNames())) || is_null($value)) {
                 continue;
             }
             $table->$field = $value;
@@ -69,30 +73,34 @@ class Migrate
 
     public function process()
     {
+        $this->_checkVersion();
+        $this->_rebuildDb();
         $this->copyUser();
         $this->copyProduct();
         $this->copyNetwork();
         $this->copySource();
+        $this->copyUserEvent();
         $this->copySystem();
         $this->copyOrganization();
         $this->copyAsset();
         $this->copyAccountLog();
         $this->copyConfiguration();
-        $this->copyRole();
         $this->copyEmailValidation();
-        $this->copyPlugin();
         $this->copyLdapConfig();
         $this->copyUpload();
         $this->copyFinding();
         $this->copyAuditLog();
         $this->copyEvidence();
+        $this->copyEvaluation();
+        $this->copyFindingEvaluation();
+        $this->calcCurrentEvaluationId();
+        $this->copyNotification();
         $this->copyUserOrganization();
         $this->copyUserRole();
     }
 
     public function copyUser()
     {
-        $this->_emptyTable('User');
         $users = self::$_db->fetchAll('SELECT * FROM users');
         foreach ($users as $row) {
             $data = $this->_migrateData($row);
@@ -105,16 +113,16 @@ class Migrate
             $data['currentLoginIp']  = $data['lastLoginIp'];
             $this->migrate('User', $data);
         }
+        $this->_printResult('User');
     }
 
     //@todo need to migrate?
     public function copyProduct()
     {
-        $this->_emptyTable('Product');
         $query = self::$_db->select()->from('products', array('count'=>'count(*)'));
         $ret   = self::$_db->fetchRow($query);
         $count = $ret['count'];
-        // prevent too many time
+        // prevent too many times
         $count = 500;
         $offset = 500;
         $query  = self::$_db->select()->from('products', array('id', 'vendor', 'name', 'version', 'cpe_name'));
@@ -126,32 +134,42 @@ class Migrate
                 $this->migrate('Product', $data);
             }
         }
+        $this->_printResult('Product');
     }
 
     public function copyNetwork()
     {
-        $this->_emptyTable('Network');
         $networks = self::$_db->fetchAll('SELECT * FROM networks');
         foreach ($networks as $row) {
             $data = $this->_migrateData($row);
             $this->migrate('Network', $data);
         }
+        $this->_printResult('Network');
     }
 
     public function copySource()
     {
-        $this->_emptyTable('Source');
         $data = array();
         $sources = self::$_db->fetchAll('SELECT * FROM sources');
         foreach ($sources as $row) {
             $data = $this->_migrateData($row);
             $this->migrate('Source', $data);
         }
+        $this->_printResult('Source');
+    }
+
+    public function copyUserEvent()
+    {
+        $userEvents = self::$_db->fetchAll('SELECT * FROM user_events');
+        foreach ($userEvents as $row) {
+            $data = $this->_migrateData($row);
+            $this->migrate('UserEvent', $data);
+        }
+        $this->_printResult('UserEvent');
     }
 
     public function copySystem()
     {
-        $this->_emptyTable('System');
         $systems = self::$_db->fetchAll('SELECT * FROM systems');
         foreach ($systems as $row) {
             $data = $this->_migrateData($row);
@@ -166,11 +184,11 @@ class Migrate
             }
             $this->migrate('System', $data);
         }
+        $this->_printResult('System');
     }
 
     public function copyOrganization()
     {
-        $this->_emptyTable('Organization');
         $organization = new Organization();
         $organizations = self::$_db->fetchAll('SELECT * FROM organizations');
         foreach ($organizations as $row) {
@@ -195,6 +213,7 @@ class Migrate
             $this->migrate('Organization', $data);
             $organization->getNode()->insertAsLastChildOf($organization->getTable()->find($row['oid']));
         }
+        $this->_printResult('Organization');
     }
 
     public function copyAsset()
@@ -209,6 +228,7 @@ class Migrate
             $data['modifiedTs']  = $row['create_ts'];
             $this->migrate('Asset', $data);
         }
+        $this->_printResult('Asset');
     }
 
     public function copyAccountLog()
@@ -249,13 +269,16 @@ class Migrate
             }
             $this->migrate('AccountLog', $data);
         }
+        $this->_printResult('AccountLog');
     }
 
     public function copyConfiguration()
     {
-        $this->_emptyTable('Configuration');
         $data = array();
         $configurations = self::$_db->fetchAll('SELECT * FROM configurations');
+        $configurations[] = array('key'=>'hash_type',
+                                  'value'=>'sha1',
+                                  'description'=>'The hash algorithm to use for password storage');
         foreach ($configurations as $row) {
             $data = $this->_migrateData($row);
             switch ($row['key']) {
@@ -279,11 +302,11 @@ class Migrate
             $data['name'] = $row['key'];
             $this->migrate('Configuration', $data);
         }
+        $this->_printResult('Configuration');
     }
 
     public function copyRole()
     {
-        $this->_emptyTable('Role');
         $data = array();
         $roles = self::$_db->fetchAll('SELECT * FROM roles');
         foreach ($roles as $row) {
@@ -292,6 +315,7 @@ class Migrate
             $data['description'] = $row['desc'];
             $this->migrate('Role', $data);
         }
+        $this->_printResult('Role');
     }
 
     public function copyEmailValidation()
@@ -305,11 +329,11 @@ class Migrate
             }
             $this->migrate('EmailValidation', $data);
         }
+        $this->_printResult('EmailValidation');
     }
 
     public function copyPlugin()
     {
-        $this->_emptyTable('Plugin');
         $plugins = self::$_db->fetchAll('SELECT * FROM plugins');
         foreach ($plugins as $row) {
             foreach ($row as $field=>$value) {
@@ -319,6 +343,7 @@ class Migrate
             }
             $this->migrate('Plugin', $data);
         }
+        $this->_printResult('Plugin');
     }
 
     public function copyLdapConfig()
@@ -328,6 +353,7 @@ class Migrate
             $data = $this->_migrateData($row);
             $this->migrate('LdapConfig', $data);
         }
+        $this->_printResult('LdapConfig');
     }
 
     public function copyUpload()
@@ -339,6 +365,7 @@ class Migrate
             $data['fileName']  = $row['filename'];
             $this->migrate('Upload', $data);
         }
+        $this->_printResult('Upload');
     }
 
     public function copyFinding()
@@ -368,6 +395,7 @@ class Migrate
             $data['duplicateFindingId']           = $row['duplicate_poam_id'];
             $data['ecdLocked']                    = empty($row['action_actual_date']) ? false : true;
             $data['responsibleOrganizationId']    = Doctrine::getTable('System')->find($row['system_id'])->Organization[0]->id;
+            $data['uploadId']                     = empty($row['upload_id']) ? null : $row['upload_id'];
             switch ($row['status']) {
                 case 'NEW':
                     $data['nextDueDate'] = date("Y-m-d",strtotime("$row[create_ts] + 30day"));
@@ -387,6 +415,7 @@ class Migrate
             }
             $this->migrate('Finding', $data);
         }
+        $this->_printResult('Finding');
     }
 
     public function copyAuditLog()
@@ -398,6 +427,7 @@ class Migrate
             $data['findingId'] = $row['poam_id'];
             $this->migrate('AuditLog', $data);
         }
+        $this->_printResult('AuditLog');
     }
 
     public function copyEvidence()
@@ -411,6 +441,83 @@ class Migrate
             $data['createdTs'] = $row['submit_ts'];
             $this->migrate('Evidence', $data);
         }
+        $this->_printResult('Evidence');
+    }
+
+    public function copyEvaluation()
+    {
+        $evaluations = self::$_db->fetchAll('SELECT * FROM evaluations order by id DESC');
+        foreach ($evaluations as $row) {
+            $data = $this->_migrateData($row);
+            if ('93' == $data['eventId']) {
+                $data['eventId'] = '53';
+            }
+            $data['approvalGroup'] = strtolower($row['group']);
+            $data['precedence']    = $row['precedence_id'];
+            $nextId = $row['id'] + 1;
+            $result = self::$_db->fetchRow("SELECT * FROM evaluations WHERE id = $nextId AND `group` = '$row[group]'");
+            if (!empty($result)) {
+                $data['nextId']  =  $nextId;
+            }
+            $this->migrate('Evaluation', $data);
+        }
+        $this->_printResult('Evaluation');
+    }
+
+    public function copyFindingEvaluation()
+    {
+        $query = self::$_db->select()->from(array('pe'=>'poam_evaluations'), 'pe.*')
+                                     ->joinLeft(array('c'=>'comments'), 'c.poam_evaluation_id = pe.id',
+                                                    array('comment'=>'c.content'))
+                                     ->join(array('e'=>'evaluations'), 'e.id = pe.eval_id',
+                                                    array('group'=>'e.group'));
+        $findingEvaluations = self::$_db->fetchAll($query);
+        foreach ($findingEvaluations as $row) {
+            $data  = $this->_migrateData($row);
+            $data['evaluationId'] = $row['eval_id'];
+            $data['createdTs']    = $row['date'];
+            if ('ACTION' == $row['group']) {
+                $data['findingId']    = $row['group_id'];
+            } elseif ('EVIDENCE' == $row['group']) {
+                $data['evidenceId']   = $row['group_id'];
+            }
+            $this->migrate('FindingEvaluation', $data);
+        }
+        $this->_printResult('FindingEvaluation');
+    }
+
+    public function calcCurrentEvaluationId()
+    {
+        $findingIds = Doctrine_Query::create()
+                        ->select('f.id')
+                        ->from('Finding f')
+                        ->execute(array(), Doctrine::HYDRATE_ARRAY);
+        foreach ($findingIds as $row) {
+            $finding = new Finding();
+            $finding = $finding->getTable()->find($row['id']);
+            if (in_array($finding->status, array('MSA', 'EA'))) {
+                $lastFindingEvaluation = $finding->FindingEvaluations->getLast();
+                if ($lastFindingEvaluation->decision == null) {
+                    $finding->currentEvaluationId = 1;
+                }
+                if ('APPROVED' == $lastFindingEvaluation->decision) {
+                    $finding->currentEvaluationId = $lastFindingEvaluation->evaluationId + 1;
+                }
+            }
+            $finding->save();
+        }
+    }
+
+    public function copyNotification()
+    {
+        $notifications = self::$_db->fetchAll('SELECT * FROM notifications');
+        foreach ($notifications as $row) {
+            $data = $this->_migrateData($row);
+            $data['createdTs'] = $row['timestamp'];
+            $data['eventId']   = null;
+            $this->migrate('Notification', $data);
+        }
+        $this->_printResult('Notification');
     }
 
     public function copyUserOrganization()
@@ -421,6 +528,7 @@ class Migrate
             $data['organizationId'] = Doctrine::getTable('System')->find($row['system_id'])->Organization[0]->id;
             $this->migrate('UserOrganization', $data);
         }
+        $this->_printResult('UserOrganization');
     }
 
     public function copyUserRole()
@@ -430,37 +538,10 @@ class Migrate
             $data = $this->_migrateData($row);
             $this->migrate('UserRole', $data);
         }
+        $this->_printResult('UserRole');
     }
 
-    public function copyEvaluation()
-    {
-        $evaluations = self::$_db->fetchAll('SELECT * FROM evaluations order by id DESC');
-        foreach ($evaluations as $row) {
-            $data = $this->_migrateData($row);
-            $data['approvalGroup'] = strtolower($row['group']);
-            $data['precedence']    = $row['precedence_id'];
-            $nextId = $row['id'] + 1;
-            $result = self::$_db->fetchRow("SELECT * FROM evaluations WHERE id = $nextId AND `group` = '$row[group]'");
-            if (!empty($result)) {
-                //$data['nextId']  =  null;
-            }
-            $this->migrate('Evaluation', $data);
-        }
-    }
 
-    public function copyFindingEvaluation()
-    {
-        $findingEvaluations = self::$_db->fetchAll('SELECT pe.* FROM poam_evaluations pe JOIN comments c on c.poam_evaluation_id = pe.id');
-        foreach ($findingEvaluations as $row) {
-            $data  = $this->_migrateData($row);
-            $data['evaluationId'] = $row['eval_id'];
-            $data['createdTs']    = $row['date'];
-            $data['comment']      = $row['comment'];
-            $data['findingId']    = $row['group_id'];
-            $data['evidenceId']   = $row['group_id'];
-            $this->migrate('FindingEvaluation', $data);
-        }
-    }
 
     /**
      * change string into a camplCase string
@@ -495,12 +576,39 @@ class Migrate
     }
 
     /**
-     * empty table data
-     *
-     * @param string $table
+     * Check the old db whether is the latest schema version
      */
-    private function _emptyTable($table)
+    private function _checkVersion()
     {
-        Doctrine::getTable($table)->findAll()->delete();        
+        $latestVersion = 62;
+        $row = self::$_db->fetchRow("SELECT schema_version FROM schema_version");
+        if ($row['schema_version'] < $latestVersion) {
+            print "Please execute the migrate.pl first";
+            exit;
+        }
+    }
+
+    /**
+     * rebuild database
+     */
+    private function _rebuildDb()
+    {
+        Doctrine::dropDatabases();
+        Doctrine::generateModelsFromYaml(Fisma::getPath('schema'), Fisma::getPath('model'));
+        Doctrine::createDatabases();
+        Doctrine::createTablesFromModels(Fisma::getPath('model'));
+        print "Rebuild Db successfully. \n";
+
+        //load sample data
+        Doctrine::loadData(Fisma::getPath('fixture'));
+        Doctrine::getTable('User')->findAll()->delete();
+        Doctrine::getTable('Source')->findAll()->delete();
+        Doctrine::getTable('Evaluation')->findAll()->delete();
+        Doctrine::getTable('Configuration')->findAll()->delete();
+    }
+    
+    private function _printResult($table)
+    {
+        print $table . " is migrated successfully. \n";        
     }
 }
