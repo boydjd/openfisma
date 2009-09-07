f05b9ba7206d3a051b606ccebf9be1950a1b51fd
Ryan Yang <ryan.yang@reyosoft.com>
2009-09-07 06:47:03 +0000

    Bug fix on the release branch: OF-44 OFJ-32: Administrator cannot edit the finding sources
    Fix some defects from Crucible
    1. Add phpdoc
    2. Lower case "Class"
    3. Use DQL update

--- application/doctrine/migrations/2_finding_resources2source.php
+++ application/doctrine/migrations/2_finding_resources2source.php
@@ -1,24 +1,59 @@
 <?php
 /**
- * Change the Privilege resource 'finding_sources' to 'source'
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenFISMA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Ryan yang <ryanyang@users.sourceforge.net>
+ * @copyright (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license   http://www.openfisma.org/mw/index.php?title=License
+ * @version   $Id: $
+ * @package   Migration
  */
-Class ModifyResource extends Doctrine_Migration_Base
+
+/**
+ * Change the Privilege resource 'finding_sources' to 'source'
+ *
+ * @package    Migration
+ * @copyright  (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license    http://www.openfisma.org/mw/index.php?title=License
+ */
+class ModifyResource extends Doctrine_Migration_Base
 {
+    /**
+     * Implement the doing changes, replace "finding_sources" with "source"
+     */
     public function up()
     {
-        $privileges = Doctrine::getTable('Privilege')->findByDql('resource = "finding_sources"');
-        foreach ($privileges as $privilege) {
-            $privilege->resource = 'source';
-            $privilege->save();
-        }
+        Doctrine_Query::create()
+            ->update('Privilege p')
+            ->set('p.resource', '?', 'source')
+            ->where('p.resource = ?', 'finding_sources')
+            ->execute();
     }
 
+    /**
+     * Implement the undoing changes, replace "source" with "finding_source"
+     */
     public function down()
     {
-        $privileges = Doctrine::getTable('Privilege')->findByDql('resource = "source"');
-        foreach ($privileges as $privilege) {
-            $privilege->resource = 'finding_sources';
-            $privilege->save();
-        }
+        Doctrine_Query::create()
+            ->update('Privilege p')
+            ->set('p.resource', '?', 'finding_sources')
+            ->where('p.resource = ?', 'source')
+            ->execute();
     }
 }
