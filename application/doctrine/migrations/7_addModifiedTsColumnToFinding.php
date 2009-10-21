a6ebcff24a59a50c258a1a484f7f92ebeecef04a
Joshua D. Boyd <joshua.boyd@endeavorsystems.com>
2009-10-21 18:46:21 +0000

    Bug fix on the release branch OFJ-140 Finding search conditions (finding satus:NOUP_30 NOUP_60 NOUP_90) dosen't work Add modifiedTs column to finding. Insert timestamps from auditlog into finding in migration #8, suspending listeners so that the Timestampable behavior doesn't override our new values.

---
+++ application/doctrine/migrations/7_addModifiedTsColumnToFinding.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenFISMA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Josh Boyd <joshua.boyd@endeavorsystems.com 
+ * @copyright (c) Endeavor Systems, Inc. 2009 (http://www.endeavorsystems.com)
+ * @license   http://openfisma.org/content/license
+ * @version   $Id$
+ * @package   Migration
+ */
+
+/**
+ * AddModifiedTsColumnToFinding 
+ * 
+ * @uses Doctrine
+ * @uses _Migration_Base
+ * @package Migration 
+ * @copyright (c) Endeavor Systems, Inc. 2009 {@link http://www.endeavorsystems.com})
+ * @author Josh Boyd <joshua.boyd@endeavorsystems.com> 
+ * @license {@link http://www.openfisma.org/content/license}
+ */
+class AddModifiedTsColumnToFinding extends Doctrine_Migration_Base
+{
+    /**
+     * up - Add ModifiedTs Column to Finding
+     * 
+     * @access public
+     * @return void
+     */
+    public function up()
+    {
+        $this->addColumn('finding', 'modifiedts', 'timestamp');
+        Doctrine::generateModelsFromYaml(Fisma::getPath('schema'), Fisma::getPath('model'));
+    }
+
+    /**
+     * down - Remove ModifiedTs column from Finding 
+     * 
+     * @access public
+     * @return void
+     */
+    public function down()
+    {
+        $this->removeColumn('finding', 'modifiedts');
+
+        Doctrine::generateModelsFromYaml(Fisma::getPath('schema'), Fisma::getPath('model'));
+    }
+}
