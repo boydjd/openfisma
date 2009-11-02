10d39ae9c837f8f950a9341ca3cd3802e92656c7
Joshua D. Boyd <joshua.boyd@endeavorsystems.com>
2009-11-02 17:24:11 +0000

    Bug fix on the release branch OFJ-279 Finding risk analysis information can't be saved
    Add an event for finding updated.

---
+++ application/doctrine/migrations/10_addFindingUpdateEvent.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenFISMA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Josh Boyd <joshua.boyd@endeavorsystems.com> 
+ * @copyright (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license   http://openfisma.org/content/license
+ * @version   $Id$
+ * @package   Migration
+ */
+
+/**
+ * Add an event that represents the deletion of a finding
+ *
+ * @package    Migration
+ * @copyright  (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
+ * @license    http://www.openfisma.org/mw/index.php?title=License
+ */
+class AddFindingUpdateEvent extends Doctrine_Migration_Base
+{
+    /**
+     * Insert new event
+     */
+    public function up()
+    {
+        // Create new event
+        $event = new Event;
+        $event->name = 'FINDING_UPDATED';
+        $event->description = 'Finding Updated';
+        
+        // Get corresponding privilege
+        $privQuery = Doctrine_Query::create()
+                     ->from('Privilege p')
+                     ->where('p.resource = ? AND p.action = ?', array('notification', 'finding'));
+        $privilege = $privQuery->fetchOne();
+        $event->Privilege = $privilege;
+        
+        $event->save();
+    }
+    
+    /**
+     * Remove new event
+     */
+    public function down()
+    {
+        Doctrine::getTable('Event')->findOneByName('FINDING_UPDATED')->delete();
+    }
+}
