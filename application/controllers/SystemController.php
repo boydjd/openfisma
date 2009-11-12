efa879dcdd0fad17046566313340a1a91ef46d12
Joshua D. Boyd <joshua.boyd@endeavorsystems.com>
2009-11-12 18:17:15 +0000

    Bug fix on the release branch
    OFJ-302 Document Type list is not sorted
    
    Sort the query by name for the IE specific view too!

--- application/controllers/SystemController.php
+++ application/controllers/SystemController.php
@@ -443,6 +443,6 @@ class SystemController extends BaseController
         }
 
         $this->view->organizationId = $id;        
-        $this->view->documentTypes = Doctrine::getTable('DocumentType')->findAll();        
+        $this->view->documentTypes  = Doctrine_Query::create()->from('DocumentType')->orderBy('name')->execute();
     }
 }
