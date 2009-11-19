e593fb9c360dbb637f7a5e63e7c166d7461d85ce
Jackson Yang <jackson.yang@reyosoft.com>
2009-11-19 05:48:36 +0000

    Documentation OFJ-291 Update all file-level and class-level phpdocs
    Update file-level and class-level phpdocs for migration scripts.

--- application/doctrine/migrations/4_convertCommentFromVarcharToClob.php
+++ application/doctrine/migrations/4_convertCommentFromVarcharToClob.php
@@ -4,32 +4,26 @@
  *
  * This file is part of OpenFISMA.
  *
- * OpenFISMA is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * OpenFISMA is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with OpenFISMA.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @author    Mark E. Haase
- * @copyright (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
- * @license   http://openfisma.org/content/license
- * @version   $Id$
- * @package   Migration
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * <http://www.gnu.org/licenses/>.
  */
 
 /**
  * The finding evaluation contains a comment field that is varchar(255). Convert it to a TEXT or CLOB field.
  *
+ * @author     Mark E. Haase <mhaase@endeavorsystems.com>
+ * @copyright  (c) Endeavor Systems, Inc. 2009 (http://www.endeavorsystems.com)
+ * @license    http://www.openfisma.org/content/license
  * @package    Migration
- * @copyright  (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
- * @license    http://www.openfisma.org/mw/index.php?title=License
+ * @version    $Id$
  */
 class ConvertCommentFromVarcharToClob extends Doctrine_Migration_Base
 {
--- application/doctrine/migrations/7_addModifiedTsColumnToFinding.php
+++ application/doctrine/migrations/7_addModifiedTsColumnToFinding.php
@@ -4,35 +4,26 @@
  *
  * This file is part of OpenFISMA.
  *
- * OpenFISMA is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * OpenFISMA is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with OpenFISMA.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @author    Josh Boyd <joshua.boyd@endeavorsystems.com 
- * @copyright (c) Endeavor Systems, Inc. 2009 (http://www.endeavorsystems.com)
- * @license   http://openfisma.org/content/license
- * @version   $Id$
- * @package   Migration
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * <http://www.gnu.org/licenses/>.
  */
 
 /**
  * AddModifiedTsColumnToFinding 
  * 
- * @uses Doctrine
- * @uses _Migration_Base
- * @package Migration 
- * @copyright (c) Endeavor Systems, Inc. 2009 {@link http://www.endeavorsystems.com})
- * @author Josh Boyd <joshua.boyd@endeavorsystems.com> 
- * @license {@link http://www.openfisma.org/content/license}
+ * @author     Josh Boyd <joshua.boyd@endeavorsystems.com>
+ * @copyright  (c) Endeavor Systems, Inc. 2009 (http://www.endeavorsystems.com)
+ * @license    http://www.openfisma.org/content/license
+ * @package    Migration
+ * @version    $Id$
  */
 class AddModifiedTsColumnToFinding extends Doctrine_Migration_Base
 {
