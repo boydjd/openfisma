5c0bf844d5844c9d0d440d1010208bbf1e4a4ade
Jackson Yang <jackson.yang@reyosoft.com>
2009-11-20 06:48:14 +0000

    Documentation OFJ-291 Update all file-level and class-level phpdocs
    Update file-level and class-level phpdocs for all php files under library/Auth, which include auth adapters and session storage.

--- library/Fisma/Auth/Adapter/Ldap.php
+++ library/Fisma/Auth/Adapter/Ldap.php
@@ -4,33 +4,27 @@
  *
  * This file is part of OpenFISMA.
  *
- * OpenFISMA is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * OpenFISMA is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with OpenFISMA.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @author    Xhorse 
- * @copyright (c) Endeavor Systems, Inc. 2008 (http://www.endeavorsystems.com)
- * @license   http://www.openfisma.org/mw/index.php?title=License
- * @version   $Id$
- * @package   Fisma_Auth
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * <http://www.gnu.org/licenses/>.
  */
 
 /**
  * Adapte the authentication to Ldap and return a Doctrine Record object as identity
  * 
- * @category   Fisma
- * @copyright  Copyright (c) 2005-2008
- * @license    http://www.openfisma.org/mw/index.php?title=License
- * @package    Fisma_Auth
+ * @author     Jim Chen <xhorse@users.sourceforge.net>
+ * @copyright  (c) Endeavor Systems, Inc. 2009 (http://www.endeavorsystems.com)
+ * @license    http://www.openfisma.org/content/license
+ * @package    Fisma
+ * @subpackage Fisma_Auth
+ * @version    $Id$
  */
 class Fisma_Auth_Adapter_Ldap extends Zend_Auth_Adapter_Ldap
 {
