2c29daea73da4986217cf36a1ec00e90bc87a922
Mark E. Haase <mhaase@endeavorsystems.com>
2010-01-28 20:06:46 +0000

    Improvement on the trunk OFJ-360 Remove doctrine hack from User.php
    The doctrine-cli script was broken due to the previous commit on this issue. This one is a little trickier than the installer. I created a temporary configuration array to use while rebuilding the db, and then after the rebuild finishes I switch it to a database-backed configuration object.
    
    This repairs the unit tests and should fix the broken build.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,7 +19,16 @@
 try {
     ini_set('memory_limit', '512M');
     Fisma::initialize(Fisma::RUN_MODE_TEST);
-    Fisma::setConfiguration(new Fisma_Configuration_Database(), true);
+    
+    /** 
+     * The bootstrap recreates all tables, including the configuration table. So to bootstrap the bootstrap, we need
+     * a temporary, in-memory configuration object. This gets replaced with a database-backed configuration object
+     * after the configuration table has been re-built.
+     */
+    $configuration = new Fisma_Configuration_Array();
+    $configuration->setConfig('hash_type', 'sha1');
+    $configuration->setConfig('session_inactivity_period', '3600');
+    Fisma::setConfiguration($configuration, true);
 
     Zend_Session::start();
 
@@ -33,6 +42,9 @@ try {
     $cliArguments = array('doctrine-cli.php', 'build-all-reload', '--no-confirmation');
     $cli->run($cliArguments);
     Fisma::setListenerEnabled(true);
+
+    // Now that the configuration table has been re-built, reset the global configuration object to use that table
+    Fisma::setConfiguration(new Fisma_Configuration_Database(), true);
 
     $frontController = Zend_Controller_Front::getInstance();
     $frontController->setControllerDirectory(Fisma::getPath('controller'));
