d2366202b01fc543df9975cefeed9765e535d192
Mark E. Haase <mhaase@endeavorsystems.com>
2010-02-02 16:41:31 +0000

    Bug on the trunk OFJ-431 When upload the same Scan Results it always shows "findings were created."
    Fixed as described in triage report -- except the getPurifier() method is already public, so I didn't bother refactoring it.

--- library/Fisma/Inject/Abstract.php
+++ library/Fisma/Inject/Abstract.php
@@ -232,10 +232,17 @@ abstract class Fisma_Inject_Abstract
      */
     private function _getDuplicateFinding($finding)
     {
+        /**
+         * In order to properly compare the current finding against persisted findings, we need to apply the same html
+         * purification that the Xss Listener applies
+         */
+        $xssListener = new XssListener();
+        $cleanDescription = $xssListener->getPurifier()->purify($finding->description);
+        
         $duplicateFindings = Doctrine_Query::create()
             ->select('f.id, f.responsibleOrganizationId, f.type, f.status')
             ->from('Finding f')
-            ->where('description LIKE ?', $finding->description)
+            ->where('description LIKE ?', $cleanDescription)
             ->setHydrationMode(Doctrine::HYDRATE_ARRAY)
             ->execute();
 
