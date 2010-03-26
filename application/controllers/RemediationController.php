fdbeef835444477c9fe6005cf9c3ca7028051a91
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2010-03-26 15:24:07 +0000

    Bug fix on branch
    OFJ-574 There are broken links on the dashboard
    
    Added escaping to DQL to match what's in the database.

--- application/controllers/RemediationController.php
+++ application/controllers/RemediationController.php
@@ -1192,7 +1192,7 @@ class RemediationController extends SecurityController
                     } elseif (in_array($v, array('NEW', 'DRAFT', 'EN', 'CLOSED'))) {
                         $q->andWhere("f.status = ?", $v);
                     } else {
-                        $q->andWhere("ce.nickname = ?", $v);
+                        $q->andWhere("ce.nickname = ?", htmlspecialchars($v, ENT_COMPAT, 'UTF-8', FALSE));
                     }
                 } elseif ($k == 'modify_ts') {
                     $v = $v->toString('Y-m-d H:i:s');
