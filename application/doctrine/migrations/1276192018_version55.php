683c0ad49578dcc51a40e4036993cc2b9b4dfb4c
Mark E. Haase <mhaase@endeavorsystems.com>
2010-06-10 18:41:24 +0000

    Bug on trunk OFJ-705 Incorrect incident search when user is actor or observer and does not have 'read incident' privilege
    Decided to do this the hard way and merge these two models. This commit contains the updated schema files and migrations.
    
    This will break any incident actor/observer stuff in the controllers, because I haven't updated those yet.

---
+++ application/doctrine/migrations/1276192018_version55.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+/**
+ * Consolidate the IrIncidentActor and IrIncidentObserver into a single model called IrIncidentUser
+ * 
+ * 1) Create a table for the IrIncidentUser
+ * 2) Copy existing actors into the new table, setting the accessType to ACTOR
+ * 3) Copy existing observers into the new table, setting the accessType to OBSERVER
+ * 
+ * It isn't possible to drop the old tables in this migration because the drop tables will run BEFORE we copy the
+ * data over in the postUp() method. (We could store the contents of the tables in a class variable, but that seems
+ * risky.) So tables are dropped in version 56.
+ * 
+ * This file contains generated code... skip standards check.
+ * @codingStandardsIgnoreFile
+ * 
+ * @package Migration
+ * @version $Id: 1268671549_version30.php 3105 2010-03-15 16:53:47Z jboyd $
+ * @copyright (c) Endeavor Systems, Inc. 2010 {@link http://www.endeavorsystems.com}
+ * @author Mark E. Haase <mhaase@endeavorsystems.com>
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Version55 extends Doctrine_Migration_Base
+{
+    /**
+     * Create the new table and drop the old tables
+     */
+    public function up()
+    {
+        // Create the new table
+		$this->createTable('ir_incident_user', array(
+             'incidentid' => 
+             array(
+              'type' => 'integer',
+              'primary' => true,
+              'length' => 8,
+             ),
+             'userid' => 
+             array(
+              'type' => 'integer',
+              'primary' => true,
+              'length' => 8,
+             ),
+             'accesstype' => 
+             array(
+              'type' => 'enum',
+              'values' => 
+              array(
+              0 => 'ACTOR',
+              1 => 'OBSERVER',
+              ),
+             ), 
+             ), array(
+             'indexes' => 
+             array(
+             ),
+             'primary' => 
+             array(
+              0 => 'incidentid',
+              1 => 'userid',
+             ),
+             ));
+        return;
+    }
+
+    /**
+     * Use postUp to migrate data from old tables to new table.
+     */
+    public function postUp()
+    {
+        // Regenerate models so that we can instantiate new IrIncidentUser objects
+        Doctrine::generateModelsFromYaml(Fisma::getPath('schema'), Fisma::getPath('doctrine-models'));
+
+        // Copy records from each old table into the new table, wrapped in a transaction
+        $conn = Doctrine_Manager::connection();
+        $conn->beginTransaction();
+        
+        try {
+            $actors = Doctrine::getTable('IrIncidentActor')->findAll();
+            
+            foreach ($actors as $actor) {
+                $incidentUser = new IrIncidentUser();
+                
+                $incidentUser->incidentId = $actor->incidentId;
+                $incidentUser->userId = $actor->userId;
+                $incidentUser->accessType = 'ACTOR';
+                
+                $incidentUser->save();
+            }
+
+            $observers = Doctrine::getTable('IrIncidentObserver')->findAll();
+            
+            foreach ($observers as $observer) {
+                $incidentUser = new IrIncidentUser();
+                
+                $incidentUser->incidentId = $observer->incidentId;
+                $incidentUser->userId = $observer->userId;
+                $incidentUser->accessType = 'OBSERVER';
+                
+                $incidentUser->save();
+            }
+            
+            $conn->commit();            
+        } catch (Doctrine_Exception $e) {
+            $conn->rollback();
+            
+            throw $e;
+        }
+    }
+
+    /**
+     * Drop the new table
+     */
+    public function down()
+    {   
+        // Drop new table
+		$this->dropTable('ir_incident_user');
+    }
+}
---
+++ application/doctrine/migrations/1276194655_version56.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+/**
+ * Finish the work started in version 55 by dropping the tables for IrIncidentActor and IrIncidentObserver
+ * 
+ * This file contains generated code... skip standards check.
+ * @codingStandardsIgnoreFile
+ * 
+ * @package Migration
+ * @version $Id: 1268671549_version30.php 3105 2010-03-15 16:53:47Z jboyd $
+ * @copyright (c) Endeavor Systems, Inc. 2010 {@link http://www.endeavorsystems.com}
+ * @author Mark E. Haase <mhaase@endeavorsystems.com>
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Version56 extends Doctrine_Migration_Base
+{
+    /**
+     * Drop the old tables
+     */
+    public function up()
+    {
+		$this->dropTable('ir_incident_actor');
+		$this->dropTable('ir_incident_observer');
+    }
+    
+    /**
+     * Recreate the old tables
+     */
+    public function down()
+    {   
+		$this->createTable('ir_incident_actor', array(
+             'incidentid' => 
+             array(
+              'type' => 'integer',
+              'primary' => true,
+              'length' => 8,
+             ),
+             'userid' => 
+             array(
+              'type' => 'integer',
+              'primary' => true,
+              'length' => 8,
+             ),
+             ), array(
+             'indexes' => 
+             array(
+             ),
+             'primary' => 
+             array(
+              0 => 'incidentid',
+              1 => 'userid',
+             ),
+             ));
+
+ 		$this->createTable('ir_incident_observer', array(
+              'incidentid' => 
+              array(
+               'type' => 'integer',
+               'primary' => true,
+               'length' => 8,
+              ),
+              'userid' => 
+              array(
+               'type' => 'integer',
+               'primary' => true,
+               'length' => 8,
+              ),
+              ), array(
+              'indexes' => 
+              array(
+              ),
+              'primary' => 
+              array(
+               0 => 'incidentid',
+               1 => 'userid',
+              ),
+              ));
+    }
+    
+    /**
+     * Use postDown to migrate data from new table back into old tables.
+     */
+    public function postDown()
+    {
+        /*
+         * Copy ACTOR records from new table into old actor table, and OBSERVER records from new table into old 
+         * observer table
+         */
+        $conn = Doctrine_Manager::connection();
+        $conn->beginTransaction();
+        
+        try {
+            $users = Doctrine::getTable('IrIncidentUser')->findAll();
+            
+            foreach ($users as $oldUser) {
+                if ('ACTOR' == $oldUser->accessType) {
+                    $newUser = new IrIncidentActor();
+                } elseif ('OBSERVER' == $oldUser->accessType) {
+                    $newUser = new IrIncidentObserver();
+                }
+                
+                $newUser->incidentId = $oldUser->incidentId;
+                $newUser->userId = $oldUser->userId;
+                
+                $newUser->save();
+            }
+            
+            $conn->commit();            
+        } catch (Doctrine_Exception $e) {
+            $conn->rollback();
+            
+            throw $e;
+        }
+    }
+}
