de4faf195b4856b930732c022cf1d5592b4e37a7
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2010-07-06 12:06:18 -0400

    Admin Updated minified/combined JS files.

--- public/javascripts/combined.js
+++ public/javascripts/combined.js
@@ -2681,7 +2681,7 @@ Fisma.Blinker=function(a,c,b,d){this.interval=a;this.cycles=c;this.cyclesRemaini
  * @author    Mark E. Haase <mhaase@endeavorsystems.com>
  * @copyright (c) Endeavor Systems, Inc. 2010 (http://www.endeavorsystems.com)
  * @license   http://www.openfisma.org/content/license
- * @version   $Id$
+ * @version   $Id: AttachArtifacts.js 3188 2010-04-08 19:35:38Z mhaase $
  */
 
 /**
@@ -2759,7 +2759,7 @@ Fisma.Commentable={asyncRequest:null,config:null,yuiPanel:null,showPanel:functio
  * @author    Mark E. Haase <mhaase@endeavorsystems.com>
  * @copyright (c) Endeavor Systems, Inc. 2010 (http://www.endeavorsystems.com)
  * @license   http://www.openfisma.org/content/license
- * @version   $Id$
+ * @version   $Id: AttachArtifacts.js 3188 2010-04-08 19:35:38Z mhaase $
  */
  
 Fisma.Commentable = {
@@ -3047,7 +3047,7 @@ Fisma.Email = function() {
         }
     };
 }();
-Fisma.Finding={commentTable:null,commentCallback:function(f,b){var d=this;var c={timestamp:f.createdTs,username:f.username,comment:f.comment};this.commentTable.addRow(c);this.commentTable.sortColumn(this.commentTable.getColumn(0),YAHOO.widget.DataTable.CLASS_DESC);var a=new Fisma.Blinker(100,6,function(){d.commentTable.highlightRow(0)},function(){d.commentTable.unhighlightRow(0)});a.start();var e=document.getElementById("findingCommentsCount").firstChild;e.nodeValue++;b.hide();b.destroy()}};/**
+Fisma.Finding={commentTable:null,commentCallback:function(f,b){var d=this;var c={timestamp:f.createdTs,username:f.username,comment:f.comment};this.commentTable.addRow(c);this.commentTable.sortColumn(this.commentTable.getColumn(0),YAHOO.widget.DataTable.CLASS_DESC);var a=new Fisma.Blinker(100,6,function(){d.commentTable.highlightRow(0)},function(){d.commentTable.unhighlightRow(0)});a.start();var e=document.getElementById("findingCommentsCount").firstChild;e.nodeValue++;b.hide();b.destroy()},editEcdJustification:function(){var a=document.getElementById("currentChangeDescription");a.style.display="none";var c;if(a.firstChild){c=a.firstChild.nodeValue}else{c=""}var b=document.createElement("input");b.type="text";b.value=c;b.name="finding[ecdChangeDescription]";a.parentNode.appendChild(b)}};/**
  * Copyright (c) 2008 Endeavor Systems, Inc.
  *
  * This file is part of OpenFISMA.
@@ -3068,7 +3068,7 @@ Fisma.Finding={commentTable:null,commentCallback:function(f,b){var d=this;var c=
  * @author    Mark E. Haase <mhaase@endeavorsystems.com>
  * @copyright (c) Endeavor Systems, Inc. 2010 (http://www.endeavorsystems.com)
  * @license   http://www.openfisma.org/content/license
- * @version   $Id$
+ * @version   $Id: AttachArtifacts.js 3188 2010-04-08 19:35:38Z mhaase $
  */
  
 Fisma.Finding = {
@@ -3139,7 +3139,12 @@ Fisma.Finding = {
         currentEcdJustificationEl.style.display = 'none';
         
         // Copy current text into a new input element
-        var currentEcdJustification = currentEcdJustificationEl.firstChild.nodeValue;
+        var currentEcdJustification;
+        if (currentEcdJustificationEl.firstChild) {
+            currentEcdJustification = currentEcdJustificationEl.firstChild.nodeValue;
+        } else {
+            currentEcdJustification = '';
+        }
         
         var inputEl = document.createElement('input');
         inputEl.type = 'text';
@@ -4409,7 +4414,7 @@ Fisma.SwitchButton.prototype = {
  * @author    Mark E. Haase <mhaase@endeavorsystems.com>
  * @copyright (c) Endeavor Systems, Inc. 2010 (http://www.endeavorsystems.com)
  * @license   http://www.openfisma.org/content/license
- * @version   $Id$
+ * @version   $Id: AttachArtifacts.js 3188 2010-04-08 19:35:38Z mhaase $
  */
  
 Fisma.System = {
@@ -4539,7 +4544,7 @@ Fisma.TableFormat={greenColor:"lightgreen",yellowColor:"yellow",redColor:"pink",
  * @author    Mark E. Haase <mhaase@endeavorsystems.com>
  * @copyright (c) Endeavor Systems, Inc. 2010 (http://www.endeavorsystems.com)
  * @license   http://www.openfisma.org/content/license
- * @version   $Id$
+ * @version   $Id: Incident.js 3288 2010-04-29 23:36:21Z mhaase $
  */
  
 Fisma.TableFormat = {
@@ -4818,7 +4823,7 @@ Fisma.Util={getObjectFromName:function(c){var b=c.split(".");var a=window;for(pi
  * @author    Mark E. Haase <mhaase@endeavorsystems.com>
  * @copyright (c) Endeavor Systems, Inc. 2010 (http://www.endeavorsystems.com)
  * @license   http://www.openfisma.org/content/license
- * @version   $Id$
+ * @version   $Id: AttachArtifacts.js 3188 2010-04-08 19:35:38Z mhaase $
  */
  
 Fisma.Util = {
