boydjd/openfisma

commit 9b37fd98c4aa37791a881509e5c4fcc56c85a627

Improvement on master OFJ-677 Information systems should have lifecycle metadata
Simplified some where clauses to clarify the queries a bit.

application/controllers/FindingController.php
@@ -75,7 +75,7 @@ class FindingController extends BaseController
         
         $systems = $this->_me->getOrganizationsByPrivilegeQuery('finding', 'create')
             ->leftJoin('o.System system')
-            ->andWhere('o.orgType <> ? OR system.sdlcPhase <> ?', array('system', 'disposal'))
+            ->andWhere('system.sdlcPhase <> ?', array('system', 'disposal'))
             ->execute();
         $selectArray = $this->view->treeToSelect($systems, 'nickname');
         $form->getElement('orgSystemId')->addMultiOptions($selectArray);

application/controllers/AssetController.php
@@ -113,7 +113,7 @@ class AssetController extends BaseController
         $form = parent::getForm($formName);
         $systems = $this->_me->getOrganizationsByPrivilegeQuery('asset', 'read')
             ->leftJoin('o.System system')
-            ->andWhere('o.orgType <> ? OR system.sdlcPhase <> ?', array('system', 'disposal'))
+            ->andWhere('system.sdlcPhase <> ?', array('disposal'))
             ->execute();
         $selectArray = $this->view->treeToSelect($systems, 'nickname');
         $form->getElement('orgSystemId')->addMultiOptions($selectArray);

application/controllers/RemediationController.php
@@ -385,7 +385,7 @@ class RemediationController extends SecurityController
             ->leftJoin('Organization parent')
             ->leftJoin('parent.System system')
             ->where('node.lft BETWEEN parent.lft and parent.rgt')
-            ->andWhere('node.orgType <> ? OR nodeSystem.sdlcPhase <> ?', array('system', 'disposal'))
+            ->andWhere('nodeSystem.sdlcPhase <> ?', array('disposal'))
             ->groupBy('parent.nickname')
             ->orderBy('parent.lft')
             ->setHydrationMode(Doctrine::HYDRATE_SCALAR);
@@ -1287,7 +1287,7 @@ class RemediationController extends SecurityController
                 'f.responsibleOrganizationId', 
                 $this->_me->getOrganizationsByPrivilege('finding', 'read')->toKeyValueArray('id', 'id')
             )
-            ->andWhere('ro.orgType <> ? OR ros.sdlcPhase <> ?', array('system', 'disposal'))
+            ->andWhere('ros.sdlcPhase <> ?', array('disposal'))
             ->orderBy($params['sortby'] . ' ' . $params['dir']);
 
         foreach ($params as $k => $v) {

commit de26c4aae1740f8f649dfd078d0da029617dc37a

OFJ-677 Information systems should have lifecycle metadata
Fix error in Finding::createAction introduced in 9b37fd98.

application/controllers/FindingController.php
@@ -75,7 +75,7 @@ class FindingController extends BaseController
         
         $systems = $this->_me->getOrganizationsByPrivilegeQuery('finding', 'create')
             ->leftJoin('o.System system')
-            ->andWhere('system.sdlcPhase <> ?', array('system', 'disposal'))
+            ->andWhere('system.sdlcPhase <> ?', array('disposal'))
             ->execute();
         $selectArray = $this->view->treeToSelect($systems, 'nickname');
         $form->getElement('orgSystemId')->addMultiOptions($selectArray);