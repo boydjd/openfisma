5019036f64b6a8af1a2669be9b1fa6e73b55e03b
Mark ma <mark.ma@reyosoft.com>
2010-08-23 02:25:03 -0400

    OFJ-835 Remove email verification feature
    remove EmailValidation.yml and validate.phtml
    remove the column emailValidate in User.yml,
    delete the codes related to email validation in user model of User.php
    delete the codes in controllers of AuthController.php and UserController.php
    delete the codes at Mail.php in mail library.
    delete the codes at sendaccountinfo.phtml in view component.
    add migration file of 1282032586_version72.php
    update the version at MigrationVersion.yml

---
+++ application/doctrine/migrations/1282032586_version72.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+/**
+ * Drop the user* emailvalidate and drop the table email_validation
+ * delete the email verification feature because it is rarely used. 
+ * 
+ * @package       Migration
+ * @version       $Id$
+ * @copyright (c) Endeavor Systems, Inc. 2009 {@link http://www.endeavorsystems.com}
+ * @author        Mark Ma <markjtma@users.sourceforge.net>
+ * @license       http://www.openfisma.org/content/license GPLv3
+ */
+class Version72 extends Doctrine_Migration_Base
+{
+    /**
+     * Drop email_validation table and drop emailvalidate column in user table
+     */
+    public function up()
+    {
+        $this->dropTable('email_validation');
+        $this->removeColumn('user', 'emailvalidate'); 
+    }
+
+    /**
+     * This migration cannot be reversed because some data is irreversably destroyed in the up() process
+     */
+    public function down()
+    {
+        throw new Doctrine_Migration_IrreversibleMigrationException();
+    }
+}
