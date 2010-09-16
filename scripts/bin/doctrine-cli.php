5cacd6b874967ca61a657b1ec9072759fd98fc18
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2010-09-16 16:53:09 -0400

    OFJ-905 Separate configuration files
    Added Zend_Application bootstrapping to doctrine-cli script.

--- scripts/bin/doctrine-cli.php
+++ scripts/bin/doctrine-cli.php
@@ -25,13 +25,32 @@
  * @license    http://www.openfisma.org/content/license GPLv3
  * @package    Scripts
  * @version    $Id$
- */
-require_once(realpath(dirname(__FILE__) . '/../../library/Fisma.php'));
-
+ r*/
 try {
     $startTime = time();
     
+    defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'production');
+    defined('APPLICATION_PATH') || define(
+        'APPLICATION_PATH',
+        realpath(dirname(__FILE__) . '/../../application')
+    );
+
+    set_include_path(
+        APPLICATION_PATH . '/../library/Symfony/Components' . PATH_SEPARATOR .
+        APPLICATION_PATH . '/../library' .  PATH_SEPARATOR .
+        get_include_path()
+    );
+
+    require_once 'Fisma.php';
+    require_once 'Zend/Application.php';
+
+    $application = new Zend_Application(
+        APPLICATION_ENV,
+        APPLICATION_PATH . '/config/application.ini'
+    );
     Fisma::initialize(Fisma::RUN_MODE_COMMAND_LINE);
+    Fisma::setAppConfig($application->getOptions());
+    $application->bootstrap();
     Fisma::connectDb();
     Fisma::setNotificationEnabled(false);
     Fisma::setListenerEnabled(false);
