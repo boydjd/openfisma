b5dce7e938a7b2697575bd7d63cd592a648e848d
Ben Zheng <ben.zheng@reyosoft.com>
2010-09-17 13:31:26 +0800

    OFJ-910 CLONE -Make all public fields in the finding model editable by end users (with appropriate permissions)
    Update migration version 76 for insert events and delete relative events

--- application/doctrine/migrations/1284516108_version76.php
+++ application/doctrine/migrations/1284516108_version76.php
@@ -17,7 +17,7 @@
  */
 
 /**
- * Insert update legacy finding key and finding source privileges
+ * Insert update legacy finding key and finding source privileges and events
  * 
  * @author     Ben Zheng <benzheng@users.sourceforge.net>
  * @copyright  (c) Endeavor Systems, Inc. 2009 {@link http://www.endeavorsystems.com}
@@ -28,62 +28,131 @@
 class Version76 extends Doctrine_Migration_Base
 {
     /**
-     * Insert privilege
+     * Insert privilege and event
      */
     public function up()
     {
-        $privileges = new Doctrine_Collection('Privilege');
+        $conn = Doctrine_Manager::connection();
 
-        $updateLegacyFindingkey = new Privilege();
-        $updateLegacyFindingkey->resource = 'finding';
-        $updateLegacyFindingkey->action = 'update_legacy_finding_key';
-        $updateLegacyFindingkey->description = 'Update Legacy Finding Key';
-        $privileges[] = $updateLegacyFindingkey;
+        $conn->beginTransaction();
+        
+        try {
+            $privileges = new Doctrine_Collection('Privilege');
 
-        $updateFindingSource = new Privilege();
-        $updateFindingSource->resource = 'finding';
-        $updateFindingSource->action = 'update_finding_source';
-        $updateFindingSource->description = 'Update Finding Source';
-        $privileges[] = $updateFindingSource;
+            $updateLegacyFindingkey = new Privilege();
+            $updateLegacyFindingkey->resource = 'finding';
+            $updateLegacyFindingkey->action = 'update_legacy_finding_key';
+            $updateLegacyFindingkey->description = 'Update Legacy Finding Key';
+            $privileges[] = $updateLegacyFindingkey;
 
-        $privileges->save();
+            $updateFindingSource = new Privilege();
+            $updateFindingSource->resource = 'finding';
+            $updateFindingSource->action = 'update_finding_source';
+            $updateFindingSource->description = 'Update Finding Source';
+            $privileges[] = $updateFindingSource;
 
-        // Assign update legacy finding key and source privileges to any role which has the update finding privilege
-        $updateFindingRolesQuery = Doctrine_Query::create()
-                                      ->from('Role r')
-                                      ->innerJoin('r.Privileges p')
-                                      ->where('p.resource = ? AND p.action like ?', array('finding', 'update_%'));
+            $privileges->save();
 
-        $updateFindingRoles = $updateFindingRolesQuery->execute();
+            // Assign update legacy finding key and source privileges to any role which has the update finding privilege
+            $updateFindingRolesQuery = Doctrine_Query::create()
+                                       ->from('Role r')
+                                       ->innerJoin('r.Privileges p')
+                                       ->where('p.resource = ? AND p.action like ?', array('finding', 'update_%'));
 
-        foreach ($updateFindingRoles as $updateFindingRole) {
-            $updateFindingRole->link('Privileges', array($updateLegacyFindingkey->id, $updateFindingSource->id));
+            $updateFindingRoles = $updateFindingRolesQuery->execute();
+    
+            foreach ($updateFindingRoles as $updateFindingRole) {
+                $updateFindingRole->link('Privileges', array($updateLegacyFindingkey->id, $updateFindingSource->id));
+            }
+
+            $updateFindingRoles->save();
+    
+            $events = new Doctrine_Collection('Event');
+
+            $findingNotication = Doctrine_Query::create()
+                                 ->from('Privilege p')
+                                 ->where('p.resource = ? AND p.action = ?', array('notification', 'finding'))
+                                 ->fetchOne();
+
+            // Add update legacy finding key and update finding source to event
+            $updateLegacyFindingkeyEvent = new Event();
+            $updateLegacyFindingkeyEvent->name = 'UPDATE_LEGACY_FINDING_KEY';
+            $updateLegacyFindingkeyEvent->description = 'Legacy Finding Key For Finding Updated';
+            $updateLegacyFindingkeyEvent->Privilege = $findingNotication;
+            $events[] = $updateLegacyFindingkeyEvent;
+
+            $updateFindingSourceEvent = new Event();
+            $updateFindingSourceEvent->name = 'UPDATE_FINDING_SOURCE';
+            $updateFindingSourceEvent->description = 'Source For Finding Updated';
+            $updateFindingSourceEvent->Privilege = $findingNotication;
+            $events[] = $updateFindingSourceEvent;
+    
+            $events->save();
+
+            $conn->commit();
+        } catch (Doctrine_Exception $e) {
+            $conn->rollback();
+
+            throw $e;
         }
-
-        $updateFindingRoles->save();
     }
 
     /**
-     * Remove privileges 
+     * Remove privileges and events
      */
     public function down()
     {
-        // Delete privilege
-        $privilegeQuery = Doctrine_Query::create()
-                          ->from('Privilege')
-                          ->where('resource = ? AND (action = ? OR action = ?)', 
-                                  array('finding','update_legacy_finding_key','update_finding_source'));
-        
-        $findingPrivileges = $privilegeQuery->execute();
-        
-        // Delete any associations those privileges have to roles
-        $deleteRolePrivilegesQuery = Doctrine_Query::create()
-                                     ->delete('RolePrivilege')
-                                     ->whereIn('privilegeid', $findingPrivileges->getPrimaryKeys());
+        $conn = Doctrine_Manager::connection();
 
-        $deleteRolePrivilegesQuery->execute();
-        
-        // Delete the privileges themselves
-        $findingPrivileges->delete();
+        $conn->beginTransaction();
+
+        try {
+            // Delete privilege
+            $privilegeQuery = Doctrine_Query::create()
+                              ->from('Privilege')
+                              ->where('resource = ?', 'finding')
+                              ->andWhereIn('action', array('update_legacy_finding_key', 'update_finding_source'));
+
+            $findingPrivileges = $privilegeQuery->execute();
+
+            // Delete any associations those privileges have to roles
+            $deleteRolePrivilegesQuery = Doctrine_Query::create()
+                                         ->delete('RolePrivilege')
+                                         ->whereIn('privilegeid', $findingPrivileges->getPrimaryKeys());
+
+            $deleteRolePrivilegesQuery->execute();
+
+            // Delete the privileges themselves
+            $findingPrivileges->delete();
+
+            // Delete events
+            $events = Doctrine_Query::create()
+                      ->from('Event')
+                      ->whereIn('name', array('UPDATE_LEGACY_FINDING_KEY', 'UPDATE_FINDING_SOURCE'))
+                      ->execute();
+
+            // Delete any associations those events have to users
+            $deleteUserEventsQuery = Doctrine_Query::create()
+                                    ->delete('UserEvent')
+                                    ->whereIn('eventid', $events->getPrimaryKeys());
+
+            $deleteUserEventsQuery->execute();
+
+            // Delete any associations those events have to notifications
+            $deleteNotificationsQuery = Doctrine_Query::create()
+                                       ->delete('Notification')
+                                       ->whereIn('eventid', $events->getPrimaryKeys());
+
+            $deleteNotificationsQuery->execute();
+
+            // Delete the events themselves
+            $events->delete();
+
+            $conn->commit();
+        } catch (Doctrine_Exception $e) {
+            $conn->rollback();
+
+            throw $e;
+        }
     }
 }
