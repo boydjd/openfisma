bf28a35da5efe68d2c63af57e72fae53f8155f76
Mark E. Haase <mehaase@gmail.com>
2010-09-25 01:02:13 -0400

    OFJ-620 Re-Design Search Feature
    Fix ampersand display in search results tables (by setting formatter to "text").

--- library/Fisma/Zend/Controller/Action/Object.php
+++ library/Fisma/Zend/Controller/Action/Object.php
@@ -498,7 +498,12 @@ abstract class Fisma_Zend_Controller_Action_Object extends Fisma_Zend_Controller
 
             $currentColumn++;
 
-            $column = new Fisma_Yui_DataTable_Column($label, $sortable, null, null, $fieldName, !$visible);
+            $column = new Fisma_Yui_DataTable_Column($label, 
+                                                     $sortable, 
+                                                     "YAHOO.widget.DataTable.formatText", 
+                                                     null, 
+                                                     $fieldName, 
+                                                     !$visible);
 
             $searchResultsTable->addColumn($column);
         }
