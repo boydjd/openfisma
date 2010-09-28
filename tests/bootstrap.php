0efd1f2a45cfe5e10e616117b49308b82a7832c6
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2010-09-28 13:22:19 -0400

    OFJ-905 Separate configuration files
    Set default environment for tests to development.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,7 +20,7 @@ try {
     defined('APPLICATION_ENV')
         || define(
             'APPLICATION_ENV',
-            (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production')
+            (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development')
         );
     defined('APPLICATION_PATH') || define(
         'APPLICATION_PATH',
