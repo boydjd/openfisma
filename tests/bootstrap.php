a631ad1f0cd10219e6bdec18c7b9473d16634a95
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2010-09-28 11:17:05 -0400

    OFJ-905 Separate configuration files
    Add application initilization to test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,6 +17,30 @@
  */
 
 try {
+    defined('APPLICATION_ENV')
+        || define(
+            'APPLICATION_ENV',
+            (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production')
+        );
+    defined('APPLICATION_PATH') || define(
+        'APPLICATION_PATH',
+        realpath(dirname(__FILE__) . '/../application')
+    );
+
+    set_include_path(
+        APPLICATION_PATH . '/../library/Symfony/Components' . PATH_SEPARATOR .
+        APPLICATION_PATH . '/../library' .  PATH_SEPARATOR .
+        get_include_path()
+    );
+
+    require_once 'Fisma.php';
+    require_once 'Zend/Application.php';
+
+    $application = new Zend_Application(
+        APPLICATION_ENV,
+        APPLICATION_PATH . '/config/application.ini'
+    );
+    Fisma::setAppConfig($application->getOptions());
     Fisma::initialize(Fisma::RUN_MODE_TEST);
 
     Doctrine_Manager::connection(new PDO('sqlite::memory:'));
