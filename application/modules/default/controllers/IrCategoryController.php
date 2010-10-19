7e0b3a7597e884cb3e98bc8dd848138f3e9febb1
Dale <dale.frey@endeavorsystems.com>
2010-10-19 17:49:11 -0400

    Reverting ofj922 upon request Revert "ofj922"
    This reverts commit 4ad1db5e2f5941997f7e35008bdede04acbbb573.

--- application/modules/default/controllers/IrCategoryController.php
+++ application/modules/default/controllers/IrCategoryController.php
@@ -179,10 +179,6 @@ class IrCategoryController extends Fisma_Zend_Controller_Action_Security
          */        
         foreach ($categories as &$category) {
             $category['children'] = $category['SubCategories'];
-
-            // OFJ-922. brackets must be escapped within the names of categories in the tree-view
-            $category['name'] = $this->view->escape($category['name'], 'html');
-            
             unset($category['SubCategories']);
             
             foreach ($category['children'] as &$child) {
