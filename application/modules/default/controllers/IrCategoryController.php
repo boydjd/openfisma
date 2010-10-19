8111225fc351d87700b343c247cd99fd1e5da68f
Dale <dale.frey@endeavorsystems.com>
2010-10-19 17:57:07 -0400

    OFJ-922 - The name can't be displayed on IR Categories/Workflows Tree View page when name contains <>
    escape() on category names before they are send to the client-browser in IrCategoryController.php
    This solves and issue where category names could not be visible when using brackets/carrots or html-like tags

--- application/modules/default/controllers/IrCategoryController.php
+++ application/modules/default/controllers/IrCategoryController.php
@@ -179,6 +179,9 @@ class IrCategoryController extends Fisma_Zend_Controller_Action_Security
          */        
         foreach ($categories as &$category) {
             $category['children'] = $category['SubCategories'];
+
+            $category['name'] = $this->view->escape($category['name'], 'html');		// Brackets must be escapped within the names of categories in the tree-view
+            
             unset($category['SubCategories']);
             
             foreach ($category['children'] as &$child) {
