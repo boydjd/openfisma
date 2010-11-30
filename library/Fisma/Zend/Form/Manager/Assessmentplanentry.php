23e76e09014a9ac2f7e1a9c66925e8d4a031fe9c
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2010-11-30 15:45:28 -0500

    OFJ-974 RMF Step 4: Assessment
    Added status and result fields to assessment-plan-entry form.

---
+++ library/Fisma/Zend/Form/Manager/Assessmentplanentry.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright (c) 2010 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+/**
+ * Fisma_Zend_Form_Manager_Assessmentplanentry
+ * 
+ * @uses Fisma_Zend_Form_Manager_Abstract
+ * @package Fisma_Zend_Form_Manager 
+ * @copyright (c) Endeavor Systems, Inc. 2010 {@link http://www.endeavorsystems.com}
+ * @author Andrew Reeves <andrew.reeves@endeavorsystems.com> 
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Fisma_Zend_Form_Manager_Assessmentplanentry extends Fisma_Zend_Form_Manager_Abstract
+{
+    /**
+     * prepareForm 
+     * 
+     * @return void
+     */
+    public function prepareForm()
+    {
+        $form = $this->getForm();
+
+        $apeTable = Doctrine::getTable('AssessmentPlanEntry');      
+
+        $statusKeys = $apeTable->getColumnDefinition('status');
+        $statusKeys = $statusKeys['values'];
+        $statusNames = array_map('ucfirst', array_map('strtolower', $statusKeys));
+        $form->getElement('status')->addMultiOptions(array_combine($statusKeys, $statusNames));
+
+        $resultElem = $form->getElement('result');
+        $resultElem->addMultiOption('');
+        $resultKeys = $apeTable->getColumnDefinition('result');
+        $resultKeys = $resultKeys['values'];
+        $resultNames = array_map('ucfirst', array_map('strtolower', $resultKeys));
+        $resultElem->addMultiOptions(array_combine($resultKeys, $resultNames));
+
+        $this->setForm($form);
+    }
+}
