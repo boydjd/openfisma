8c228068cbfe46fad5ceae5065fa3e6065cc8dc7
Josh Boyd <joshua.boyd@endeavorsystems.com>
2010-12-28 17:57:25 -0500

    OFJ-1081 Upgrade YUI to 2.8.2
    Upgrade YUI to 2.8.2r1 for security fixes

---
+++ application/doctrine/migrations/1293576186_version92.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright (c) 2008 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+/**
+ * Update YUI version 
+ * 
+ * @package Migration
+ * @version $Id$
+ * @copyright (c) Endeavor Systems, Inc. 2010 {@link http://www.endeavorsystems.com}
+ * @author Josh Boyd <joshua.boyd@endeavorsystems.com> 
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Version92 extends Doctrine_Migration_Base
+{
+    /**
+     * Add configuration
+     */
+    public function up()
+    {
+        $yui = Doctrine::getTable('Configuration')->findOneByName('yui_version');
+        $yui->value = '2.8.2';
+        $yui->save();
+        $yui->free();
+        unset($yui);
+    }
+
+    /**
+     * Remove configuration 
+     */
+    public function down()
+    {
+        $yui = Doctrine::getTable('Configuration')->findOneByName('yui_version');
+        $yui->value = '2.8.0r4';
+        $yui->save();
+        $yui->free();
+        unset($yui);
+    }
+}
