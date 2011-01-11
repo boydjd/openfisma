87e6715dc213718ae764735ea3707098f542f392
Josh Boyd <joshua.boyd@endeavorsystems.com>
2011-01-11 14:22:01 -0500

    OFJ-1112 Add unit tests
    Add unit tests for SystemDocument and Upload models

---
+++ tests/application/models/SystemDocument.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright (c) 2011 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+require_once(realpath(dirname(__FILE__) . '/../../FismaUnitTest.php'));
+
+/**
+ * Test_Application_Models_SystemDocument
+ * 
+ * @uses Test_FismaUnitTest
+ * @package Test 
+ * @copyright (c) Endeavor Systems, Inc. 2011 {@link http://www.endeavorsystems.com}
+ * @author Josh Boyd <joshua.boyd@endeavorsystems.com> 
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Test_Application_Models_SystemDocument extends Test_FismaUnitTest
+{
+    /**
+     * testGetSizeKb 
+     * 
+     * @access public
+     * @return void
+     */
+    public function testGetSizeKb()
+    {
+        $doc = new SystemDocument();
+
+        $doc->size = 10485760;
+
+        $this->assertEquals('10240 KB', $doc->getSizeKb());
+    }
+
+    /**
+     * testGetIconUrlForUnknownFile 
+     * 
+     * @access public
+     * @return void
+     */
+    public function testGetIconUrlForUnknownFile()
+    {
+        $doc = new SystemDocument();
+
+        $doc->fileName = 'lolwat';
+
+        $this->assertEquals('/images/mimetypes/unknown.png', $doc->getIconUrl());
+    }
+
+    /**
+     * testGetIconUrlForKnownFile 
+     * 
+     * @access public
+     * @return void
+     */
+    public function testGetIconUrlForKnownFile()
+    {
+        $doc = new SystemDocument();
+
+        $doc->fileName = 'lolwat.doc';
+
+        $this->assertEquals('/images/mimetypes/doc.png', $doc->getIconUrl());
+    }
+}
---
+++ tests/application/models/SystemDocumentTable.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) 2011 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+require_once(realpath(dirname(__FILE__) . '/../../FismaUnitTest.php'));
+
+/**
+ * Test_Application_Models_SystemDocumentTable 
+ * 
+ * @uses Test_FismaUnitTest
+ * @package Test_ 
+ * @copyright (c) Endeavor Systems, Inc. 2011 {@link http://www.endeavorsystems.com}
+ * @author Josh Boyd <joshua.boyd@endeavorsystems.com> 
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Test_Application_Models_SystemDocumentTable extends Test_FismaUnitTest
+{
+    /**
+     * testGetSearchableFields 
+     * 
+     * @access public
+     * @return void
+     */
+    public function testGetSearchableFields()
+    {
+        $searchableFields = SystemDocumentTable::getSearchableFields();
+
+        $this->assertTrue(is_array($searchableFields));
+        $this->assertEquals(7, count($searchableFields));
+    }
+
+    /**
+     * testGetAclFields 
+     * 
+     * @access public
+     * @return void
+     */
+    public function testGetAclFields()
+    {
+        $this->assertTrue(is_array(SystemDocumentTable::getAclFields()));
+    }
+}
