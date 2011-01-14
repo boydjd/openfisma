b927df1295b95782f6bde5a453f1795530e3741f
Josh Boyd <joshua.boyd@endeavorsystems.com>
2011-01-14 15:33:31 -0500

    OFJ-1112 Add unit tests
    Add basic tests for services

---
+++ tests/application/services/Acl.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright (c) 2011 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+require_once(realpath(dirname(__FILE__) . '/../../FismaUnitTest.php'));
+
+/**
+ * Test_Application_Services_Acl
+ * 
+ * @uses Test_FismaUnitTest
+ * @package Test 
+ * @copyright (c) Endeavor Systems, Inc. 2011 {@link http://www.endeavorsystems.com}
+ * @author Josh Boyd <joshua.boyd@endeavorsystems.com> 
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Test_Application_Services_Acl extends Test_FismaUnitTest
+{
+    /**
+     * testSet 
+     * 
+     * @access public
+     * @return void
+     */
+    public function testSet()
+    {
+        $acl = new Application_Service_Acl();
+        $this->assertAttributeEmpty('_acl', $acl);
+        $acl->set(new Fisma_Zend_Acl());
+        $this->assertAttributeInstanceOf('Fisma_Zend_Acl', '_acl', $acl);
+    }
+
+    /**
+     * testGet 
+     * 
+     * @access public
+     * @return void
+     */
+    public function testGet()
+    {
+        $acl = new Application_Service_Acl();
+        $acl->set(new Fisma_Zend_Acl());
+        $this->assertInstanceOf('Fisma_Zend_Acl', $acl->get());
+    }
+}
---
+++ tests/application/services/CurrentUser.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright (c) 2011 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+require_once(realpath(dirname(__FILE__) . '/../../FismaUnitTest.php'));
+
+/**
+ * Test_Application_Services_CurrentUser
+ * 
+ * @uses Test_FismaUnitTest
+ * @package Test 
+ * @copyright (c) Endeavor Systems, Inc. 2011 {@link http://www.endeavorsystems.com}
+ * @author Josh Boyd <joshua.boyd@endeavorsystems.com> 
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Test_Application_Services_CurrentUser extends Test_FismaUnitTest
+{
+    /**
+     * testSet 
+     * 
+     * @access public
+     * @return void
+     */
+    public function testSet()
+    {
+        $currentUser = new Application_Service_CurrentUser();
+        $this->assertAttributeEmpty('_user', $currentUser);
+        $currentUser->set(new User());
+        $this->assertAttributeInstanceOf('User', '_user', $currentUser);
+    }
+
+    /**
+     * testGet 
+     * 
+     * @access public
+     * @return void
+     */
+    public function testGet()
+    {
+        $currentUser = new Application_Service_CurrentUser();
+        $currentUser->set(new User());
+        $this->assertInstanceOf('User', $currentUser->get());
+    }
+}
