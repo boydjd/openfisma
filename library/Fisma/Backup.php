db4b179b26c41a60fcda63701c194aea362b3b6f
Dale Frey <dale.frey@endeavorsystems.com>
2011-02-15 14:45:49 -0500

    Revert "OFJ-1171 Refactor backup.php"
    Reverting commit that was supposed to go onto seperate branch.
    This reverts commit 3fc5b1659fef65702d5ae353e34096146578b152.

--- library/Fisma/Backup.php
+++ library/Fisma/Backup.php
@@ -1,35 +1,23 @@
 <?php
 
-class Fisma_Cli_Backup extends Fisma_Cli_Abstract
+class Fisma_Backup
 {
-    // default values while config has not yet been loaded       
+     // default values while config has not yet been loaded
+    public $config;         
     private $_myTimeStamp;   // the time stamp to name the backup folder with
     private $_appRoot;       // the root openfisma directory that contains the application, public, library, etc dirs
     private $_backupRoot;    // the directory that holds all openfisma backups
     private $_backupDir;     // the directory to place the this MySQLDump and this openfisma copy into
-
-    /**
-     * Configure the arguments accepted for this CLI program
-     *
-     * @return array An array containing getopt long syntax
-     */
-    public function getArgumentsDefinitions()
-    {
-        return array(
-            'dir|d=s' => "Target directory to place backups.",
-            'compress|c' => 'A flag that states the backup should be compressed.',
-            'age|a=i' => 'The max age to keep other backups found in the backup directory'
-        );
-    }
-
-    protected function _run()
-    {
     
+    public function doBackup()
+    {
         // Root check (based on permissions, running this script out of root may not work when copying all files)
         if (trim(strtolower(`whoami`)) !== 'root') {
-            print "\nWARNING - You are running a backup script outside of root, if you receive errors " .
+            $this->echoDebug(
+                "\nWARNING - You are running a backup script outside of root, if you receive errors " .
                 "during the backup process, then;\n open a terminal\n cd to openfisma\n ". 
-                "and type: sudo php -f scripts/bin/backup.php\n\n";
+                "and type: sudo php -f scripts/bin/backup.php\n\n"
+            );
         }
         
         // Time changes in seconds, remember the current time
@@ -37,21 +25,23 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
         
         // config vars
         $this->_appRoot = realpath(APPLICATION_PATH . '/../');
-        print "Application directory is; " . $this->_appRoot . "\n";
+        $this->echoDebug("Application directory is; " . $this->_appRoot . "\n");
         
-        if (is_null($this->getOption('dir'))) {
-            print "Fatal Error - backup_directory is not defined." .
-                "Please define state the target backup directory with the -d option.\n" . 
-                "See -h for more help.\n";
+        if (!isset($this->config["backup"]["backup_directory"])) {
+            $this->echoDebug(
+                "Fatal Error - backup_directory is not defined in the configuration file! " .
+                "Please define backup.backup_directory in your config file and make sure it is valid.\n"
+            );
             return false;
         } else {
-            $this->_backupRoot = $this->getOption('dir');
+            $this->_backupRoot = $this->config["backup"]["backup_directory"];
             @mkdir($this->_backupRoot);                  // make sure the parent directory to _backupDir really exists
             if (file_exists($this->_backupRoot) === false) {
-                print "Fatal Error - backup_directory directory pointer ($this->_backupRoot) is invalid!\n";
+                $this->echoDebug("Fatal Error - backup_directory directory pointer ($this->_backupRoot) is invalid!");
                 return false;
             }
             $this->_backupRoot = realpath($this->_backupRoot);
+            $this->echoDebug("Backup directory is; " . $this->_backupRoot . "\n");
         }
 
         // Declare $_backupDir, based on _backupRoot + timestamp(), and create the directory
@@ -59,10 +49,10 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
         $this->_backupDir = str_replace("//", "/", $this->_backupDir);
         
         if (!@mkdir($this->_backupDir)) {
-            print "Fatal Error - Could not create backup directory ($this->_backupDir)\n";
+            $this->echoDebug("Fatal Error - Could not create backup directory ($this->_backupDir)");
             return false;
         }
-        print "Backup directory is; $this->_backupDir\n";
+        $this->echoDebug("Backup directory is; $this->_backupDir\n");
         
         // Remove outdated backups
         if ($this->pruneBackups() === false) {
@@ -78,34 +68,40 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
         // compress backup this directory is the settings say so
         $this->compressBackup();
         
-        print "Backup completed successfully!\n";
+        $this->echoDebug("Backup completed successfully!\n");
         return true;
     }
     
     function copyApplication()
     {
-        print "Backing up application, please wait...\n";
-        print "   Copying $this->_appRoot to $this->_backupDir...\n";
+        $this->echoDebug("Backing up application, please wait...");
+        $this->echoDebug("   Copying $this->_appRoot to $this->_backupDir...");
         $this->recursive_copy($this->_appRoot, $this->_backupDir, "   ");
-        print "   done.\n";
+        $this->echoDebug("   done.");
     }
     
     function compressBackup()
     {
-        $optCompress = $this->getOption('compress');
         
-        if (!is_null($optCompress) && $optCompress === true) {
+        if (!isset($this->config['backup']['compress'])) {
+            $this->echoDebug(
+                'WARNING: backup.compress is not defined in your configuration file. This backup ' .
+                'process will assume false, and not compress the backup.'
+            );
+        }
+        
+        if ($this->config['backup']['compress'] == true) {
             $tgzPath = $this->_backupRoot . '/' . $this->_myTimeStamp . ".tgz";
             $tgzPath = str_replace('//', '/', $tgzPath);
-            print "Compressing backup into " . $tgzPath . "\n";
+            $this->echoDebug("Compressing backup into " . $tgzPath . "\n");
             chdir($this->_backupRoot);
             $s = exec("tar -zcpf " . $tgzPath . " " . $this->_myTimeStamp . "/");
             $s = exec("rm -r " . $this->_myTimeStamp . "/");
             if (!file_exists($tgzPath)) {
-                print "   compress failed.\n";
+                $this->echoDebug("   compress failed.");
                 return false;
             }
-            print "   done.\n";
+            $this->echoDebug("   done.\n");
         }
         
     }
@@ -119,7 +115,7 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
         
         $dirHandle = @opendir($dirsource);
         if ($dirHandle === false) {
-            print $debugIndent . "copy failed for dir;  $dirsource \n";
+            $this->echoDebug($debugIndent . "copy failed for dir;  $dirsource \n");
             return false;
         }
         
@@ -130,7 +126,7 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
             if ($file !== "." && $file !== "..") {
                 if (!is_dir($dirsource . "/" . $file)) {
                     if (!@copy($dirsource . "/" . $file, $dirdest . "/" . $dirname . "/" . $file)) {
-                        print $debugIndent . "copy failed for file; " . $dirsource . "/" . $file . "\n";
+                        $this->echoDebug($debugIndent . "copy failed for file; " . $dirsource . "/" . $file . "\n");
                     }
                 } else {
                     $dirdest1 = $dirdest . "/" . $dirname;
@@ -149,27 +145,18 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
             Dumps a copy of the specified schema into a file inside the backup directory
         */
         
-        // Get MySql login info
-        $dbConfig = new Zend_Config_Ini(Fisma::getPath('application') . '/config/database.ini');
-        $db = $dbConfig->toArray();
-        $db = $db['production']['db'];
-        $dbUser = $db['username'];
-        $dbPass = $db['password'];
-        $dbSchema = $db['schema'];
-        
-        print "Backing up schema, please wait...\n";
+        $this->echoDebug("Backing up schema, please wait...");
         $backupFileSql = $this->_backupDir . "schema.sql";
-        print "   Target file will be $backupFileSql\n";
+        $this->echoDebug("   Target file will be $backupFileSql");
         
-        $mySqlDumpCmd =
-            "mysqldump --user=" . $dbUser . 
-            " --password=" . $dbPass . 
-            " --add-drop-database" . 
-            " --compact " . $dbSchema;
+        $mySqlDumpCmd = "mysqldump --user=" . $this->config['db']['username'];
+        $mySqlDumpCmd .= " --password=" . $this->config['db']['password'];
+        $mySqlDumpCmd .= " --add-drop-database";
+        $mySqlDumpCmd .= " --compact " . $this->config['db']['schema'];
         $schema = shell_exec($mySqlDumpCmd);
         file_put_contents($backupFileSql, $schema);
         
-        print "   done.\n";
+        $this->echoDebug("   done.");
     }
     
     function pruneBackups()
@@ -180,14 +167,18 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
             Returns an array file/directories that were removed successfully
         */
         
-        print "Removing outdated backups...\n";
+        $this->echoDebug("Removing outdated backups...\n");
         
         // Verify prude config exists
-        if (is_null($this->getOption('age'))) {
-            return true;
+        if (!isset($this->config["backup"]["retentionPeriod"])) {
+            $this->echoDebug(
+                "Fatal Error - backup.retentionPeriod is not defined in the configuration file! " .
+                "Please define retentionPeriod in your config file and make sure it is valid.\n"
+            );
+            return false;
         } else {
-            $retentionPeriod = $this->getOption('age');
-            print '   Backups older than ' . $retentionPeriod . " days will be removed\n";
+            $retentionPeriod = $this->config["backup"]["retentionPeriod"];
+            $this->echoDebug('   Backups older than ' . $retentionPeriod . ' days will be removed');
         }
         
         // Dont prune backups?
@@ -224,20 +215,23 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
                     
                     if (is_dir($oldBackupName)) {
                         
-                        print "   Removing old backup directory created on " . date("F j, Y", $oldTime) . "\n";
-                        
+                        $this->echoDebug(
+                            "   Removing old backup directory created on " . date("F j, Y", $oldTime) . "\n"
+                        );
                         $s = exec("rm -R $oldBackupName/");
                         if (file_exists("$oldBackupName/")) {
-                            print "   directory deletion failed: " . realpath($oldBackupName) . "\n";
+                            $this->echoDebug("   directory deletion failed: " . realpath($oldBackupName) . "\n");
                         } else {
                             $rtn[] = realpath("$oldBackupName/");
                         }
                         
                     } else {
                         
-                        print "   Removing old backup archive created on " . date("F j, Y", $oldTime) . "\n";
+                        $this->echoDebug(
+                            "   Removing old backup archive created on " . date("F j, Y", $oldTime) . "\n"
+                        );
                         if (!unlink($oldBackupName)) {
-                            print "   archive deletion failed: " . realpath($oldBackupName) . "\n";
+                            $this->echoDebug("   archive deletion failed: " . realpath($oldBackupName) . "\n");
                         } else {
                             $rtn[] = realpath($oldBackupName);
                         }
@@ -249,7 +243,56 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
             
         }
         
-        print "   done\n";
+        $this->echoDebug("   done");
+    }
+
+    function getConfig($configPath)
+    {
+        /*
+            String[] getConfig(string)
+            Reads the contents of the file-path inputted and returns an array
+            Returns false if the given input path does not exist
+        */
+        
+        $rtnArray = Array('debug' => true, 'htmlOutput' => false);  // defaults
+        
+        // verify there is a config file exists
+        if (!file_exists($configPath)) {
+            return false;
+        }
+        
+        // load config file
+        $fileLines = file($configPath);
+        // take out CRs, or LFs left behind which file() sometimes does
+        $fileLines = str_replace(chr(10), "", $fileLines);
+        // take out CRs, or LFs left behind which file() sometimes does
+        $fileLines = str_replace(chr(13), "", $fileLines);
+        
+        foreach ($fileLines as $thisLine) {                         // for each line in the file
+            
+            if (substr($thisLine, 0, 1) !== "#") {                 // is the line not start with # (comment)
+                if (strpos($thisLine, "=") !== false) {            // does this line have a; something = something?
+                    
+                    $thisLineParts = explode("=", $thisLine);   // divide the line by "=" as the delimiter
+                    $readField = trim($thisLineParts[0]);
+                    $readValue = trim($thisLineParts[1]);
+                    
+                    if (strtolower($readValue) === 'true') {    // convert string to bool
+                        $readValue = true;
+                    }
+                    if (strtolower($readValue) === 'false') {   // convert string to bool
+                        $readValue = false;
+                    }
+                    
+                    // note the read value as an element in the return array with the key of read-field
+                    $rtnArray[$readField] = $readValue;
+                    
+                }
+            }
+
+        }
+
+        return $rtnArray;
     }
         
     function timestamp()
@@ -262,4 +305,37 @@ class Fisma_Cli_Backup extends Fisma_Cli_Abstract
     
         return date("YmdHis");
     }
+    
+    function echoDebug($debugText)
+    {
+        /**
+            void echoDebugInfo()
+            Echoes input, only if the debug flag is marked true in the config file
+            Can echo in HTML format for future use
+            @returns nothing
+        */
+        
+        // defaults
+        if (!isset($this->config['backup']['debug'])) {
+            $this->config['backup']['debug'] = true;
+        }
+        if (!isset($this->config['backup']['htmlOutput'])) {
+            $this->config['backup']['htmlOutput'] = false;
+        }
+        
+        if ($this->config['backup']['debug'] !== false) {
+            if ($this->config['backup']['htmlOutput'] !== false) {
+                $debugText = str_replace("\n", "<br/>", $debugText);
+                $debugText = str_replace(" ", "&nbsp;", $debugText);
+                echo $debugText;
+            } else {
+                if (strpos($debugText, "\n") === false) {
+                    $debugText .= "\n";
+                }                
+                echo $debugText;
+            }
+        }
+        
+    }
+    
 }
