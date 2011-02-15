cf86c12e5d9a9637e9c5f3ddab14f59c2c3f7d31
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2011-02-15 10:02:51 -0500

    OFJ-1174 Add "Go To..." option for models commonly referenced by ID
    Add generic "Go To" javascript event handler.

--- public/javascripts/Fisma/Menu.js
+++ public/javascripts/Fisma/Menu.js
@@ -16,6 +16,38 @@
                     }
                 }
             }
+        },
+
+        goTo: function(eType, eObject, param) {
+            // create dialog
+            var Dom = YAHOO.util.Dom,
+                Event = YAHOO.util.Event,
+                Panel = YAHOO.widget.Panel,
+                container = document.createElement('form'),
+                textField = document.createElement('input'),
+                button = document.createElement('input');
+            Dom.setAttribute(textField, "type", "text");
+            Dom.setAttribute(button, "type", "button");
+            Dom.setAttribute(button, "value", "Go");
+            container.innerHTML = "ID: ";
+            container.appendChild(textField);
+            container.appendChild(button);
+
+            // Add event listener
+            var fn = function(ev, obj) {
+                var url = obj.controller + "/view/id/" + obj.textField.value;
+                window.location = url;
+            };
+            param.textField = textField;
+            Event.addListener(button, "click", fn, param);
+
+            // show the panel
+            var panel = new Panel(Dom.generateId(), {modal: true});
+            panel.setHeader("Go To " + param.model + "...");
+            panel.setBody(container);
+            panel.render(document.body);
+            panel.center();
+            panel.show();
         }
     };
 })();
