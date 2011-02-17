1f2732e24dea9f55af5732f14c1099fee482d7ec
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2011-02-17 08:46:30 -0500

    OFJ-1179 The search result is incorrect when click the link on a record whose status is "EV IV&V" or "MS IV&V" on overdue finding report page
    Added new JS utility function for unescaping HTML entities.
    Unescaped HTML entities within the Fisma.TableFormat.overdueFinding function.

--- public/javascripts/Fisma/Util.js
+++ public/javascripts/Fisma/Util.js
@@ -116,5 +116,17 @@ Fisma.Util = {
         }
 
         return hours + ":" + minutes + ":" + seconds;
+    },
+
+    /**
+     * Unescape HTML entities within a string.
+     */
+    unescapeEntities: function (input) {
+        var temp = document.createElement("div");
+        temp.innerHTML = input;
+        var result = temp.childNodes[0].nodeValue;
+        temp.removeChild(temp.firstChild);
+        return result;
     }
+
 };
