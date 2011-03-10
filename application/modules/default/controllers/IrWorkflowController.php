e1e5fa9ba409cc82e69886ea9ea2e3f7e4ce4c34
Josh Boyd <joshua.boyd@endeavorsystems.com>
2011-03-10 12:27:51 -0500

    OFJ-1228 A XSS Vulnerability found on ir-workflow edit page after update role nickname on the role edit page
    Escape the roles before they are added to the form

--- application/modules/default/controllers/IrWorkflowController.php
+++ application/modules/default/controllers/IrWorkflowController.php
@@ -149,6 +149,10 @@ class IRWorkflowController extends Fisma_Zend_Controller_Action_Object
 
         $roles = $rolesQuery->execute()->toKeyValueArray('id', 'nickname');
 
+        foreach ($roles as &$role) {
+            $role = $this->view->escape($role);
+        }
+
         // Get workflow steps
         $stepsQuery = Doctrine_Query::create()
                       ->from('IrStep')
