920160378bbe4f3e14c6ea2296bab6a2647fa3ff
Josh Boyd <joshua.boyd@endeavorsystems.com>
2011-03-28 17:38:49 -0400

    OFJ-1274 Enable TinyMCE Paste Plugin
    Turn on the paste plugin

--- public/javascripts/combined.js
+++ public/javascripts/combined.js
@@ -757,7 +757,7 @@ tinyMCE.init({
 	mode : "textareas",
 	cleanup : false,
 	element_format : "html",
-	plugins : "spellchecker, searchreplace, insertdatetime, print, fullscreen",
+	plugins : "paste, spellchecker, searchreplace, insertdatetime, print, fullscreen",
 	plugin_insertdate_dateFormat : "%Y-%m-%d",
 	plugin_insertdate_timeFormat : "%H:%M:%S",
 	browsers : "msie,gecko,safari,opera",
@@ -777,7 +777,7 @@ tinyMCE.init({
 	theme_advanced_statusbar_location : "bottom",
 	theme_advanced_resizing : true,
 	spellchecker_rpc_url : '/javascripts/tiny_mce/plugins/spellchecker/rpc.php',
-	spellchecker_languages : "+English=en"
+	spellchecker_languages : "+English=en",
 });
 /**
  * Copyright (c) 2008 Endeavor Systems, Inc.
