3cf254c50a10ebf1335cb0270143173e60d64253
Dale Frey <dale.frey@endeavorsystems.com>
2011-04-12 15:02:52 -0400

    OFJ-1287 Chart bugs (and hot fixes applied to FDIC)
    Fixed a bug where the Open Findings By Organization chart on the Findings Dashboard never loads when setting the Display By options to System
    on the FDIC servers.
    The reason for this was due to how many systems the FDIC has, and how many queries were done for each system (which leads to a execution timeout).
    This issue has been fixed by creating a more efficient query for the "Open Findings By Organization" chart for this specific mode.

--- application/modules/finding/controllers/DashboardController.php
+++ application/modules/finding/controllers/DashboardController.php
@@ -245,41 +245,107 @@ class Finding_DashboardController extends Fisma_Zend_Controller_Action_Security
         $threatLevel = urldecode($this->getRequest()->getParam('threatLevel'));
         $threatLevel = strtolower($threatLevel);
         
-        if ($displayBy === 'everything') {
-            
-            /* TODO: remove this, Everything is no longer an option/mode for this chart */
-            $rtnChart = $this->_chartfindingorgbasic();
-            
-        } else {
-        
-            $rtnChart = new Fisma_Chart();
-            $rtnChart
-                ->setThreatLegendVisibility(true)
-                ->setThreatLegendWidth(450)
-                ->setAxisLabelY('Number of Findings')
-                ->setChartType('stackedbar')
-                ->setColors(
-                    array(
-                        "#FF0000",
-                        "#FF6600",
-                        "#FFC000"
-                    )
+        $rtnChart = new Fisma_Chart();
+        $rtnChart
+            ->setThreatLegendVisibility(true)
+            ->setThreatLegendWidth(450)
+            ->setAxisLabelY('Number of Findings')
+            ->setChartType('stackedbar')
+            ->setColors(
+                array(
+                    "#FF0000",
+                    "#FF6600",
+                    "#FFC000"
                 )
-                ->setLayerLabels(
-                    array(
-                        'Null',
-                        'HIGH',
-                        'MODERATE',
-                        'LOW'
-                    )
-                );
+            )
+            ->setLayerLabels(
+                array(
+                    'Null',
+                    'HIGH',
+                    'MODERATE',
+                    'LOW'
+                )
+            );
+            
+        // Dont query if there are no organizations this user can see
+        $visibleOrgs = FindingTable::getOrganizationIds();
+        if (empty($visibleOrgs)) {
+            $this->view->chart = $rtnChart->export('array');
+            return;
+        }
+        
+        $basicLink =
+            '/finding/remediation/list?q=' . 
+            '/denormalizedStatus/textDoesNotContain/CLOSED' . 
+            '/organization/organizationSubtree/';
+        
+        if ($displayBy === 'system') {
+            
+            /* Because of the number of systems this query involvs, and the fact
+                that Systems shouldnt have children (unlike Bureaus for example) 
+                a different query will be used here */
                 
-                // Dont query if there are no organizations this user can see
-                $visibleOrgs = FindingTable::getOrganizationIds();
-                if (empty($visibleOrgs)) {
-                    $this->view->chart = $rtnChart->export('array');
-                    return;
+                $q = Doctrine_Query::create();
+                $q
+                    ->addSelect('COUNT(f.id), o.nickname, f.threatLevel')
+                    ->from('Finding f')
+                    ->leftJoin('f.ResponsibleOrganization o')
+                    ->where('o.orgtype = "system"')
+                    ->whereIn('o.id ', FindingTable::getOrganizationIds())
+                    ->groupBy('o.nickname, f.threatLevel')
+                    ->orderBy('o.nickname')
+                    ->setHydrationMode(Doctrine::HYDRATE_SCALAR);
+                $results = $q->execute();
+                
+                $findingCounts = array('NULL' => 0, 'HIGH' => 0, 'MODERATE' => 0, 'LOW' => 0);
+                foreach ($results as $result) {
+                    
+                    $orgName = $result['o_nickname'];
+                    
+                    if (!empty($lastResultOrg) && $result['o_nickname'] !== $lastResultOrg) {
+                        // then all high/mod/low counts for the lastResultOrg organization have been scanned through
+                        
+                        $rtnChart->addColumn(
+                            $orgName,
+                            array_values($findingCounts),
+                            array(
+                                '',
+                                $basicLink . $orgName . '/threatLevel/enumIs/HIGH',
+                                $basicLink . $orgName . '/threatLevel/enumIs/MODERATE',
+                                $basicLink . $orgName . '/threatLevel/enumIs/LOW'
+                            )
+                        );
+                        
+                        $findingCounts = array('Null' => 0, 'HIGH' => 0, 'MODERATE' => 0, 'LOW' => 0);
+                        $lastOrgChartted = $orgName;
+                    }
+                    
+                    if (in_array($result['f_threatLevel'], $findingCounts)) {
+                        // findingCounts [ of this threatLevel ] = number of findings of this threatLevel
+                        $findingCounts[$result['f_threatLevel']] = $result['f_COUNT'];
+                    } else {
+                        $findingCounts['NULL'] = $result['f_COUNT'];
+                    }
+                    
+                    $lastResultOrg = $orgName;
+                    
                 }
+
+                // Was the last organization in the results array added to the chart?
+                if ($orgName !== $lastOrgChartted) {
+                    $rtnChart->addColumn(
+                        $orgName,
+                        $findingCounts,
+                        array(
+                            '',
+                            $basicLink . $thisParentOrg['o_nickname'] . '/threatLevel/enumIs/HIGH',
+                            $basicLink . $thisParentOrg['o_nickname'] . '/threatLevel/enumIs/MODERATE',
+                            $basicLink . $thisParentOrg['o_nickname'] . '/threatLevel/enumIs/LOW'
+                        )
+                    );
+                }                
+                                
+        } else {
 
             // Get a list of requested organization-parent types (Agency-organizations, Bureau-organizations, gss, etc)
             $parents = $this->_getOrganizationsByOrgType($displayBy);
@@ -292,19 +358,14 @@ class Finding_DashboardController extends Fisma_Zend_Controller_Action_Security
                 // Do not use association, high/mod/low is defined on the chart with Fisma_Chart->setLayerLabels()
                 $childrenTotaled = array_values($childrenTotaled);
 
-                $basicLink =
-                    '/finding/remediation/list?q=' . 
-                    '/denormalizedStatus/textDoesNotContain/CLOSED' . 
-                    '/organization/organizationSubtree/' . $thisParentOrg['nickname'];
-
                 $rtnChart->addColumn(
                     $thisParentOrg['nickname'],
                     $childrenTotaled,
                     array(
                         '',
-                        $basicLink . '/threatLevel/enumIs/HIGH',
-                        $basicLink . '/threatLevel/enumIs/MODERATE',
-                        $basicLink . '/threatLevel/enumIs/LOW'
+                        $basicLink . $thisParentOrg['nickname'] . '/threatLevel/enumIs/HIGH',
+                        $basicLink . $thisParentOrg['nickname'] . '/threatLevel/enumIs/MODERATE',
+                        $basicLink . $thisParentOrg['nickname'] . '/threatLevel/enumIs/LOW'
                     )
                 );
 
@@ -943,7 +1004,7 @@ class Finding_DashboardController extends Fisma_Zend_Controller_Action_Security
             $toDayStr = $toDay->toString('YYY-MM-dd');
             
             if ($x !== count($dayRange) - 2) {
-                $fromDay->addDay(-1);
+                $fromDay->addday(-1);
                 $fromDayStr = $fromDay->toString('YYY-MM-dd');
                 $fromDayInt--;
             }
