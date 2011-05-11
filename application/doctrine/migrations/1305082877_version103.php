a8f9b66799e876d72026d7c3f198dba9b7700200
Mark ma <mark.ma@reyosoft.com>
2011-05-10 23:05:49 -0400

    OFJ-1285 Add a "must reset password" field to user model, and set it to true for root user fixture
    merge master into ofj1285, change migration version number

--- application/doctrine/migrations/1305082877_version103.php
+++ application/doctrine/migrations/1305082877_version103.php
@@ -26,7 +26,7 @@
  * @author Mark Ma <mark.ma@reyosoft.com>
  * @license http://www.openfisma.org/content/license GPLv3
  */
-class Version100 extends Doctrine_Migration_Base
+class Version103 extends Doctrine_Migration_Base
 {
     /**
     * add mustresetpassword to user table
