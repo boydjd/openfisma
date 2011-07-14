54f11eb3032690f1b5c143f71dc2f9211bf9bb01
Josh Boyd <joshua.boyd@endeavorsystems.com>
2011-07-14 12:49:45 -0400

    OFJ-1390 Move app_version and yui_version out of configuration table
    Resolve migration conflict

--- application/doctrine/migrations/1310662121_version118.php
+++ application/doctrine/migrations/1310662121_version118.php
@@ -25,7 +25,7 @@
  * @author Andrew Reeves <andrew.reeves@endeavorsystems.com>
  * @license http://www.openfisma.org/content/license GPLv3
  */
-class Version117 extends Doctrine_Migration_Base
+class Version118 extends Doctrine_Migration_Base
 {
     public function up()
     {
