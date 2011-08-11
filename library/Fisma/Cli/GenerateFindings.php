fee07570dd3363f77bf58975499eec2969368b3f
Ben Zheng <ben.zheng@reyosoft.com>
2011-08-11 11:36:07 +0800

    OFJ-1379 An error message occurs when run generate-finding.php script without sample data in CLI
    Change Fisma_Exception to Fisma_Zend_Exception_User.

--- library/Fisma/Cli/GenerateFindings.php
+++ library/Fisma/Cli/GenerateFindings.php
@@ -102,7 +102,7 @@ class Fisma_Cli_GenerateFindings extends Fisma_Cli_Abstract
                               ->execute();
 
         if (0 == count($this->_sampleUsers)) {
-            throw new Fisma_Exception("Cannot generate sample data because the application has no users.");
+            throw new Fisma_Zend_Exception_User("Cannot generate sample data because the application has no users.");
         }
 
         // Get the evaluation ID for MSA
