2374f3615770c00b195c3d8da5070b430f35a5dc
Mark E. Haase <mhaase@endeavorsystems.com>
2011-11-02 18:40:19 -0400

    OFJ-1560 Make countermeasures not null and not blank, set default value to LOW
    Update model to make cmeasures not nullable, enforce not blank constraint in the model. (Notice that
    mysql cannot enforce a not blank constraint on an enum field.)

---
+++ application/doctrine/migrations/1320271991_version129.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright (c) 2011 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+/**
+ * Modify countermeasures so that it cannot be null.
+ * 
+ * @uses Doctrine_Migration_Base
+ * @package Migration
+ * @copyright (c) Endeavor Systems, Inc. 2011 {@link http://www.endeavorsystems.com}
+ * @author Mark E. Haase <mhaase@endeavorsystems.com> 
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Version129 extends Doctrine_Migration_Base
+{
+    /**
+     * Remove nullability from countermeasures 
+     */
+    public function up()
+    {
+		$this->changeColumn('finding', 'countermeasureseffectiveness', '', 'enum', array(
+             'values' => 
+             array(
+              0 => 'LOW',
+              1 => 'MODERATE',
+              2 => 'HIGH',
+             ),
+             'notnull' => '1',
+             'extra' => 
+             array(
+              'auditLog' => '1',
+              'logicalName' => 'Effectiveness of Countermeasures',
+             ),
+             'comment' => 'A subjective assessment of the effectivness of the in-place countermeasures against the described threat',
+             ));
+    }
+
+    /**
+     * Add nullability for countermeasures
+     * 
+     * @return void
+     */
+    public function down()
+    {
+		$this->changeColumn('finding', 'countermeasureseffectiveness', '', 'enum', array(
+             'values' => 
+             array(
+              0 => 'LOW',
+              1 => 'MODERATE',
+              2 => 'HIGH',
+             ),
+             'notnull' => '0',
+             'extra' => 
+             array(
+              'auditLog' => '1',
+              'logicalName' => 'Effectiveness of Countermeasures',
+             ),
+             'comment' => 'A subjective assessment of the effectivness of the in-place countermeasures against the described threat',
+             ));
+    }
+}
+
