2344a2bde41d20879cfc9c479a742ab8c13b1341
Duy K. Bui <duy.bui@endeavorsystems.com>
2012-01-27 12:19:36 -0500

    OFJ-1322 Make Select Menus in the Application Use YUI Button Styling
    Decorator done.

--- library/Fisma/Zend/Form/Manager.php
+++ library/Fisma/Zend/Form/Manager.php
@@ -4,15 +4,15 @@
  *
  * This file is part of OpenFISMA.
  *
- * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see
  * {@link http://www.gnu.org/licenses/}.
  */
 
@@ -20,7 +20,7 @@
  * Provides an interface for loading forms that takes care of setting up the
  * common aspects of all forms used in OpenFISMA, such as standard decorators,
  * validators, and filters.
- * 
+ *
  * @author     Mark E. Haase <mhaase@endeavorsystems.com>
  * @copyright  (c) Endeavor Systems, Inc. 2009 {@link http://www.endeavorsystems.com}
  * @license    http://www.openfisma.org/content/license GPLv3
@@ -32,13 +32,13 @@ class Fisma_Zend_Form_Manager
     /**
      * Loads a specified form by looking in the standard forms
      * directory.
-     * 
+     *
      * @param string $formName The name of the form to load. This form should
      * exist inside the forms directory. (Do not include the '.form' file
      * extension.)
      * @return Zend_Form The loaded zend form
      */
-    static function loadForm($formName) 
+    static function loadForm($formName)
     {
         // Load the form from a .form file
         $front = Zend_Controller_Front::getInstance()->getRequest();
@@ -55,12 +55,12 @@ class Fisma_Zend_Form_Manager
         }
 
         $form = new Fisma_Zend_Form();
-        
+
         // Configure this form to use custom form elements
         $form->addPrefixPath('Fisma_Zend_Form', 'Fisma/Zend/Form', 'element');
         $form->addPrefixPath('Fisma_Zend_Form_Decorator', 'Fisma/Zend/Form/Decorator', 'decorator');
         $form->addPrefixPath('Fisma_Yui', 'Fisma/Yui', 'element');
-        
+
         $form->addElementPrefixPath('Fisma_Zend_Form_Validate', 'Fisma/Zend/Form/Validate', 'validate');
 
         $form->setConfig($config);
@@ -70,14 +70,14 @@ class Fisma_Zend_Form_Manager
 
    /**
      * Adds the standard decorators and filters to the specified form.
-     * 
+     *
      * @param Zend_Form $form The specifed zend form to be decorated
      * @return Zend_Form The modified form
      */
-    static function prepareForm(Fisma_Zend_Form $form, array $options = null) 
+    static function prepareForm(Fisma_Zend_Form $form, array $options = null)
     {
         $form->setMethod('post');
-        
+
         // Use the Decorator for all Display Groups and Elements
         $form->setDecorators(
             array(
@@ -106,19 +106,24 @@ class Fisma_Zend_Form_Manager
             unset($prepareForm);
         }
 
-        // By default, all input is trimmed of extraneous white space
+        // Specifically handle some special element types
         foreach ($form->getElements() as $element) {
+            // By default, all input is trimmed of extraneous white space
             if (!$element->getFilter('StringTrim')) {
                 $element->addFilter('StringTrim');
             }
+            // Add decorator for select element
+            if ($element->getType() == 'Zend_Form_Element_Select') {
+                $element->addDecorator(new Fisma_Zend_Form_Decorator_YuiSelect);
+            }
         }
-        
+
         return $form;
     }
 
     /**
      * Get form errors if form validate false
-     * 
+     *
      * @param Zend_From $form The zend form which fails to be validated and includes error messages
      * @return string The form errors
      * @todo this error display code needs to go into the decorator,
