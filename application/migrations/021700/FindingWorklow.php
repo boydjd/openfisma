7439fbc8235d9c65dd44c8d59ad9013c52ed3469
Duy K. Bui <duy.bui@endeavorsystems.com>
2012-02-22 14:01:51 -0500

    OFJ-1725 Migration for Finding Workflow Administartive View
    Including Evaluation name changes from OFJ-1696.

---
+++ application/migrations/021700/FindingWorklow.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) 2012 Endeavor Systems, Inc.
+ *
+ * This file is part of OpenFISMA.
+ *
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see
+ * {@link http://www.gnu.org/licenses/}.
+ */
+
+/**
+ * Application_Migration_021700_FindingWorkflow
+ *
+ * @uses Fisma_Migration_Abstract
+ * @package Migration
+ * @copyright (c) Endeavor Systems, Inc. 2012 {@link http://www.endeavorsystems.com}
+ * @author Duy K. Bui <duy.bui@endeavorsystems.com>
+ * @license http://www.openfisma.org/content/license GPLv3
+ */
+class Application_Migration_021700_UpdateEvaluationNames extends Fisma_Migration_Abstract
+{
+    /**
+     * migrate
+     *
+     * @return void
+     */
+    public function migrate()
+    {
+        $this->getHelper()->exec(
+            'ALTER TABLE `evaluation` '
+            . 'ADD COLUMN `description` text NULL AFTER `nickname`, '
+            . 'ADD COLUMN `deleted_at` datetime NULL AFTER `daysuntildue`;'
+        );
+    }
+}
+
