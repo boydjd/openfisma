1b825bd4988b23f632e364bdd814c4bdf5b758c5
Duy K. Bui <duy.bui@endeavorsystems.com>
2012-03-20 15:33:21 -0400

    OFJ-1768 A XSS vulnerability found on finding workflow page
    Fix

--- public/javascripts/Fisma/FindingWorkflow.js
+++ public/javascripts/Fisma/FindingWorkflow.js
@@ -213,6 +213,14 @@ Fisma.FindingWorkflow = {
                         var inputs = jQuery('#finding_workflow_select_roles input:checked');
 
                         jQuery(linkElement).prev().html(inputs.parents('span').text().replace(/.\n/g, '<br/>'));
+                        jQuery(linkElement).prev().html(
+                            jQuery.map(
+                                inputs.parents('span').text().split("\n"),
+                                function(val) {
+                                    return (val == '') ? null : $("<div>").text(val).html();
+                                }
+                            ).join("<br/>")
+                        );
 
                         var roles = "";
                         jQuery.each(inputs, function(i, e){roles += e.name + "|";});
