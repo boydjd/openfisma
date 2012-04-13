5f025738819b9f292932e1dff33ae8dceb08c778
Duy K. Bui <duy.bui@endeavorsystems.com>
2012-04-13 11:43:25 -0400

    OFJ-1779
    Merged with master
    Resolve migration conflicts

--- application/models/Finding.php
+++ application/models/Finding.php
@@ -387,6 +387,12 @@ class Finding extends BaseFinding implements Fisma_Zend_Acl_OrganizationDependen
      */
     private function _updateNextDueDate()
     {
+        if (Fisma::RUN_MODE_COMMAND_LINE != Fisma::mode() && Fisma::RUN_MODE_TEST != Fisma::mode()) {
+            $config = Fisma::configuration();
+            $this->_overdue['NEW'] = $config->getConfig('finding_new_due');
+            $this->_overdue['DRAFT'] = $config->getConfig('finding_draft_due');
+        }
+
         if (in_array($this->status, array('PEND', 'CLOSED'))) {
             $this->_set('nextDueDate', null);
             return;
