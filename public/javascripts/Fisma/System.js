ec6ea1e17e17ac5c55c6fb59019da804102d3e08
Duy K. Bui <duy.bui@endeavorsystems.com>
2012-04-18 08:14:16 -0400

    OFJ-1792 The tooltip does not show correctly
    Fixed for double panels

--- public/javascripts/Fisma/System.js
+++ public/javascripts/Fisma/System.js
@@ -193,6 +193,8 @@ Fisma.System = {
             panelConfig
         );
 
+        setTimeout(function () {jQuery('body').addClass('masked');}, 100);
+
         panel.hideEvent.subscribe(function (e) {
             setTimeout(function () {panel.destroy();}, 0);
         });
