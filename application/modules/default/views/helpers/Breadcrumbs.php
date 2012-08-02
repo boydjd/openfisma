cef719b7546659d1fe51887657b541c82c1de6b8
xue-wei tang <xue-wei.tang@endeavorsystems.com>
2012-08-02 07:41:09 -0400

    OFJ-1886 Implement a simple breadcrumb
    Refactor the code to implement the breadcrumb in a helper class.

--- application/modules/default/views/helpers/Breadcrumbs.php
+++ application/modules/default/views/helpers/Breadcrumbs.php
@@ -4,21 +4,21 @@
  *
  * This file is part of OpenFISMA.
  *
- * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see
  * {@link http://www.gnu.org/licenses/}.
  */
 
 /**
  * A view helper which renders breadcrumbs
- * 
+ *
  * @author     Xue-Wei Tang <xue-wei.tang@endeavorystems.com>
  * @copyright  (c) Endeavor Systems, Inc. 2010 {@link http://www.endeavorsystems.com}
  * @license    http://www.openfisma.org/content/license GPLv3
@@ -35,61 +35,60 @@ class View_Helper_Breadcrumbs extends Zend_View_Helper_Abstract
     private static function tok2Array($str)
     {
         $a = array();
-    
+
         $tok = strtok($str, ",");
         while ($tok !== false) {
             array_push($a, $tok);
             $tok = strtok(",");
         }
-    
+
         return $a;
     }
-    
+
     /**
      * Return the breadcrumbs for the given URL
      *
-     * @param $url the page URL 
+     * @param $url the page URL
      *
      * @return string the breadcrumb <div> tag
      */
      public function breadcrumbs($url)
     {
         $this->breadcrumbInfo = array();
-        
+
         // Load the breadcrumbs configuration file
         $path = Fisma::getPath('config');
         $this->breadcrumbInfo = Doctrine_Parser_YamlSf::load($path . '/breadcrumbs.yml');
-        
+
         $breadcrumbs = null;
-        
+
         // Find the item that matches the given URL.
         $item = null;
         foreach ($this->breadcrumbInfo as $key => $val) {
             $item = $this->breadcrumbInfo[$key];
-        
+
             $urlItem = $item["url"];
             if (strpos($url, $urlItem) !== false) {
                 // Found matching URL
                 break;
             }
         }
-        
+
         // The given URL is not found in the breadcrumbs configuration
         if (!isset($item) ||
-             strpos($url, "dashboard") !== false ||
              strpos($url, "phpinfo") !== false)
         {
             $breadcrumbs = "<div id=\"breadcrumbs\"></div>";
             return $breadcrumbs;
         }
-        
+
         // Process item found from the breadcrumbs configuration
         $labels = $item["breadcrumb-labels"];
         $links = $item["breadcrumb-links"];
-        
+
         $labelItems = self::tok2Array($labels);
         $linkItems  = self::tok2Array($links);
-        
+
         // Construct the breadcrumbs
         $breadcrumbs =
         "<div id=\"breadcrumbs\" style=\"padding: 10px 10px 0 30px; font-weight: bold; font-size: 1.0em;\">";
@@ -100,14 +99,14 @@ class View_Helper_Breadcrumbs extends Zend_View_Helper_Abstract
             else {
                 $breadcrumbs .= "<a href=\"$linkItems[$i]\">$labelItems[$i]</a>";
             }
-        
+
             if ($i < count($labelItems) - 1) {
                 $breadcrumbs .= "<img src=\"/images/bullet_raquo.gif\" style=\"padding: 0 10px 0 10px;\">";
             }
         }
-        
-        $breadcrumbs .= ":</div>";
-        
+
+        $breadcrumbs .= "</div>";
+
         return $breadcrumbs;
     }
 }
