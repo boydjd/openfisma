6ed3ed3f3bf6559bba68649965a0d2b250f3e806
xue-wei tang <xue-wei.tang@endeavorsystems.com>
2012-08-24 13:26:34 -0400

    OFJ-1956 Bulk upload for vulnerabilities scan
    Moved the vulnerability to next release based on code review feedback.

--- application/migrations/021801/AddEvent.php
+++ application/migrations/021801/AddEvent.php
@@ -54,30 +54,6 @@ class Application_Migration_021801_AddEvent extends Fisma_Migration_Abstract
            WHERE resource = 'notification'
              AND action   = 'finding'"
         );
-
-        $this->message("Adding VULNERABILITY_IMPORT event...");
-        $this->getHelper()->exec(
-                "INSERT INTO event (
-                 name
-                ,description
-                ,privilegeid
-                ,urlpath
-                ,category
-                ,defaultactive
-                ,deleted_at
-             )
-             SELECT
-                'VULNERABILITY_IMPORTED'
-                ,'vulnerabilities are imported'
-                ,id
-                ,'/vm/vulnerability/list'
-                ,'vulnerability'
-                ,1
-                ,null
-            FROM privilege
-           WHERE resource = 'notification'
-             AND action   = 'vulnerability'"
-        );
     }
 }
 
