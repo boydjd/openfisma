5ea1c24f4f7cad6b7b35f629d375d6b0caee03c4
Duy K. Bui <duy.bui@endeavorsystems.com>
2012-09-04 10:18:38 -0400

    OFJ-1967 Incident Module Search Improvement

--- application/models/IncidentTable.php
+++ application/models/IncidentTable.php
@@ -78,6 +78,95 @@ class IncidentTable extends Fisma_Doctrine_Table implements Fisma_Search_Searcha
                 'type' => 'datetime',
                 'formatter' => 'datetime'
             ),
+            'reporter' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporting User',
+                'join' => array(
+                    'model' => 'User',
+                    'relation' => 'ReportingUser',
+                    'field' => 'displayName'
+                ),
+                'sortable' => true,
+                'type' => 'text'
+            ),
+            'reporterIp' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter IP Address',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterTitle' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter Title',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterFirstName' => array(
+                'initiallyVisible' => false,
+                'label' => 'Annonymous Reporter First Name',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterLastName' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter Last Name',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterOrganization' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter Organization',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterAddress1' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter Address Line 1',
+                'type' => 'text',
+                'sortable' => false
+            ),
+            'reporterAddress2' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter Address Line 2',
+                'type' => 'text',
+                'sortable' => false
+            ),
+            'reporterCity' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter City',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterState' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter State',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterZip' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter ZIP',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterPhone' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter Phone Number',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterFax' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter Fax Number',
+                'type' => 'text',
+                'sortable' => true
+            ),
+            'reporterEmail' => array(
+                'initiallyVisible' => false,
+                'label' => 'Reporter Email Address',
+                'type' => 'text',
+                'sortable' => true
+            ),
             'status' => array(
                 'enumValues' => $this->getEnumValues('status'),
                 'initiallyVisible' => true,
@@ -110,6 +199,18 @@ class IncidentTable extends Fisma_Doctrine_Table implements Fisma_Search_Searcha
             'additionalInfo' => array(
                 'initiallyVisible' => true,
                 'label' => 'Description',
+                'sortable' => true,
+                'type' => 'text'
+            ),
+            'locationBuilding' => array(
+                'initiallyVisible' => false,
+                'label' => 'Building',
+                'sortable' => true,
+                'type' => 'text'
+            ),
+            'locationRoom' => array(
+                'initiallyVisible' => false,
+                'label' => 'Room',
                 'sortable' => true,
                 'type' => 'text'
             ),
@@ -174,9 +275,93 @@ class IncidentTable extends Fisma_Doctrine_Table implements Fisma_Search_Searcha
             'piiInvolved' => array(
                 'enumValues' => $this->getEnumValues('piiInvolved'),
                 'initiallyVisible' => true,
-                'label' => 'PII Involved',
+                'label' => 'PII Involved?',
                 'sortable' => true,
                 'type' => 'enum'
+            ),
+            'piiAdditional' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Additional Information',
+                'sortable' => false,
+                'type' => 'text'
+            ),
+            'piiMobileMedia' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Mobile Media Involed?',
+                'sortable' => true,
+                'enumValues' => $this->getEnumValues('piiMobileMedia'),
+                'type' => 'enum'
+            ),
+            'piiMobileMediaType' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Mobile Media Type',
+                'sortable' => true,
+                'type' => 'text'
+            ),
+            'piiEncrypted' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Encrypted?',
+                'sortable' => true,
+                'enumValues' => $this->getEnumValues('piiEncrypted'),
+                'type' => 'enum'
+            ),
+            'piiAuthoritiesContacted' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Authorities Contacted?',
+                'sortable' => true,
+                'enumValues' => $this->getEnumValues('piiAuthoritiesContacted'),
+                'type' => 'enum'
+            ),
+            'piiPoliceReport' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Police Report?',
+                'sortable' => true,
+                'enumValues' => $this->getEnumValues('piiPoliceReport'),
+                'type' => 'enum'
+            ),
+            'piiIndividualsCount' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Individuals Count',
+                'type' => 'integer'
+            ),
+            'piiIndividualsNotified' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Individuals Notified?',
+                'sortable' => true,
+                'enumValues' => $this->getEnumValues('piiIndividualsNotified'),
+                'type' => 'enum'
+            ),
+            'piiShipment' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Shipment Involved?',
+                'sortable' => true,
+                'enumValues' => $this->getEnumValues('piiShipment'),
+                'type' => 'enum'
+            ),
+            'piiShipmentSenderContacted' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Shipment Sender Contacted?',
+                'sortable' => true,
+                'enumValues' => $this->getEnumValues('piiShipmentSenderContacted'),
+                'type' => 'enum'
+            ),
+            'piiShipmentSenderCompany' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Shipment Sender Company',
+                'sortable' => true,
+                'type' => 'text'
+            ),
+            'piiShipmentTimeline' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Shipment Timeline',
+                'sortable' => false,
+                'type' => 'text'
+            ),
+            'piiShipmentTrackingNumbers' => array(
+                'initiallyVisible' => false,
+                'label' => 'PII Shipment Tracking Numbers',
+                'sortable' => true,
+                'type' => 'text'
             ),
             'hostIp' => array(
                 'initiallyVisible' => false,
@@ -197,10 +382,22 @@ class IncidentTable extends Fisma_Doctrine_Table implements Fisma_Search_Searcha
                 'sortable' => true,
                 'type' => 'enum'
             ),
+            'hostAdditional' => array(
+                'initiallyVisible' => false,
+                'label' => 'Host Additional Information',
+                'sortable' => false,
+                'type' => 'text'
+            ),
             'sourceIp' => array(
                 'initiallyVisible' => false,
                 'label' => 'Source IP',
                 'sortable' => true,
+                'type' => 'text'
+            ),
+            'sourceAdditional' => array(
+                'initiallyVisible' => false,
+                'label' => 'Source Additional Information',
+                'sortable' => false,
                 'type' => 'text'
             ),
             'closedTs' => array(
@@ -209,6 +406,11 @@ class IncidentTable extends Fisma_Doctrine_Table implements Fisma_Search_Searcha
                 'sortable' => true,
                 'type' => 'datetime',
                 'formatter' => 'datetime'
+            ),
+            'reportingUserId' => array(
+                'initiallyVisible' => false,
+                'type' => 'integer',
+                'hidden' => true
             )
         );
     }
