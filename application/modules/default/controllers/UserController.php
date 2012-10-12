8bf145c1890ca8a4bef5d22c29ea694d1ad939f8
Duy K. Bui <duy.bui@endeavorsystems.com>
2012-10-12 08:29:31 -0400

    OFJ-2000 User form submission redirection
    Move the form to view.phtml and submit to /view/ instead of /user/

--- application/modules/default/controllers/UserController.php
+++ application/modules/default/controllers/UserController.php
@@ -733,9 +733,12 @@ class UserController extends Fisma_Zend_Controller_Action_Object
      */
     public function viewAction()
     {
+        parent::_viewObject();
+
         $id = $this->_request->getParam('id');
         $subject = $this->_getSubject($id);
         $fromSearchParams = $this->_getFromSearchParams($this->_request);
+        $fromSearchUrl = $this->_helper->makeUrlParams($fromSearchParams);
         $tabView = new Fisma_Yui_TabView('FindingView', $id);
 
         $commentCount = '<span id=\'commentsCount\'>' . $this->_getSubject($id)->getComments()->count() . '</span>';
@@ -752,7 +755,10 @@ class UserController extends Fisma_Zend_Controller_Action_Object
         $this->view->modelName = $this->getSingularModelName();
         $this->view->toolbarButtons = $toolbarButtons;
         $this->view->searchButtons = $searchButtons;
+        $this->view->id = $id;
+        $this->view->fromSearchUrl = $fromSearchUrl;
     }
+
     /**
      * Show user details
      *
@@ -806,7 +812,6 @@ class UserController extends Fisma_Zend_Controller_Action_Object
         $this->view->roles = Zend_Json::encode($roles);
 
         parent::_viewObject();
-
         $this->view->form->removeDecorator('Fisma_Zend_Form_Decorator');
     }
 
