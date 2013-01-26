302510caf1de7bede46fa532be03eeaaa7edd0f6
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2013-01-25 19:23:20 -0500

    Admin: Second go at fixing test case.

--- tests/application/models/Asset.php
+++ tests/application/models/Asset.php
@@ -4,34 +4,34 @@
  *
  * This file is part of OpenFISMA.
  *
- * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see
  * {@link http://www.gnu.org/licenses/}.
  */
 
 require_once(realpath(dirname(__FILE__) . '/../../Case/Unit.php'));
 
 /**
- * Test_Application_Models_Asset 
- * 
+ * Test_Application_Models_Asset
+ *
  * @uses Test_Case_Unit
  * @package Test
  * @copyright (c) Endeavor Systems, Inc. 2011 {@link http://www.endeavorsystems.com}
- * @author Josh Boyd <joshua.boyd@endeavorsystems.com> 
+ * @author Josh Boyd <joshua.boyd@endeavorsystems.com>
  * @license http://www.openfisma.org/content/license GPLv3
  */
-class Test_Application_Models_Asset extends Test_Case_Unit
+class Test_Application_Models_Asset extends Test_Case_Database
 {
     /**
-     * testGetOrganizationDependencyId 
-     * 
+     * testGetOrganizationDependencyId
+     *
      * @access public
      * @return void
      */
@@ -45,7 +45,7 @@ class Test_Application_Models_Asset extends Test_Case_Unit
 
     /**
      * Test preDelete()
-     * 
+     *
      * @return void
      */
     public function testPreDelete()
@@ -55,7 +55,7 @@ class Test_Application_Models_Asset extends Test_Case_Unit
 
         $mockVulnerability = $this->getMock('Mock_Blank', array('set'));
         $asset->Vulnerabilities[] = $mockVulnerability;
-        
+
         $this->setExpectedException('Fisma_Zend_Exception_User',
                                     'This asset cannot be deleted because it has vulnerabilities against it');
         $asset->preDelete(null); // expects exception as Vulnerability has been added
