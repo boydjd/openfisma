972eb82c5c1b4bb60d11e0e6db79d378bd10d61d
Duy K. Bui <duy.bui@endeavorsystems.com>
2013-02-15 15:52:51 -0500

    OFJ-2113

--- application/models/Incident.php
+++ application/models/Incident.php
@@ -431,7 +431,8 @@ class Incident extends BaseIncident
             } catch (Zend_Json_Exception $e) {
                 throw new Fisma_Zend_Exception("Invalid value for response strategies.", $e);
             }
-        } else if (is_array($value)) {
+        }
+        if (is_array($value)) {
             // no special processing required
         } else {
             $value = array();
