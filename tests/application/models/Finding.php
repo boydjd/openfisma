7b6caf7fc8a610888987d58e5562906ca44c5cde
Duy K. Bui <duy.bui@endeavorsystems.com>
2013-03-06 10:03:51 -0500

    Admin: Fix Unit tests for OFJ-1868

--- tests/application/models/Finding.php
+++ tests/application/models/Finding.php
@@ -30,74 +30,6 @@ require_once(realpath(dirname(__FILE__) . '/../../Case/Unit.php'));
 class Test_Application_Models_Finding extends Test_Case_Unit
 {
     /**
-     * Cannot set next due date directly
-     *
-     * @return void
-     * @expectedException Fisma_Zend_Exception
-     */
-    public function testCannotSetDueDateDirectly()
-    {
-        $finding = new Finding();
-
-        $finding->nextDueDate = Zend_Date::now();
-    }
-
-    /**
-     * New findings should be NEW status and setting the type should move the finding to DRAFT
-     *
-     * @return void
-     * @throws PHPUnit_Framework_ExpectationFailedException if assertion fails
-     */
-    public function testFindingStatus()
-    {
-        $finding = new Finding();
-
-        $this->assertEquals('NEW', $finding->status);
-
-        $finding->type = 'CAP';
-
-        $this->assertEquals('DRAFT', $finding->status);
-    }
-
-    /**
-     * Test due dates being set automatically based on status
-     *
-     * @return void
-     * @throws PHPUnit_Framework_ExpectationFailedException if assertion fails
-     */
-    public function testDueDatesForPendAndClosedFindings()
-    {
-        $finding = new Finding();
-
-        // If the finding is set to PEND, the date should be null
-        $finding->status = 'PEND';
-        $this->assertNull($finding->nextDueDate);
-
-        // If the finding is set to CLOSED, the date should be null
-        $finding->status = 'CLOSED';
-        $this->assertNull($finding->nextDueDate);
-    }
-
-    /**
-     * Test due date logic for new and draft findings based on the creation timestamp
-     */
-    public function testDueDatesForNewAndDraftFindings($value='')
-    {
-        $finding = new Finding();
-
-        // Manipulate the creation timestamp to 45 days prior to today
-        $finding->createdTs = Zend_Date::now()->subDay(45)->toString(Zend_Date::ISO_8601);
-
-        // Trigger the finding to update its due date by setting the status again
-        $finding->status = 'NEW';
-
-        // Expected due date is 30 days after creation date
-        $expectedDueDate = Zend_Date::now()->subDay(15)->toString(Fisma_Date::FORMAT_DATE);
-
-        $this->assertEquals($expectedDueDate, $finding->nextDueDate);
-    }
-
-    /**
      * Original ECD cannot be set directly
      *
      * @return void
@@ -136,53 +68,6 @@ class Test_Application_Models_Finding extends Test_Case_Unit
 
         $this->assertEquals($date2, $finding->currentEcd);
         $this->assertNotEquals($date2, $finding->originalEcd);
-    }
-
-    /**
-     * When the status goes to EN, the ecd should be locked
-     *
-     * @return void
-     * @throws PHPUnit_Framework_ExpectationFailedException if assertion fails
-     */
-    public function testEcdLockEnStatus()
-    {
-        $finding = new Finding();
-
-        // Unlocked by default
-        $this->assertFalse($finding->ecdLocked);
-
-        // Should be locked when the status is EN
-        $finding->currentEcd = Fisma::now();
-        $finding->status = 'EN';
-        $this->assertTrue($finding->ecdLocked);
-
-        // Even setting the status back to DRAFT should not unlock it
-        $finding->status = 'DRAFT';
-        $this->assertTrue($finding->ecdLocked);
-    }
-
-    /**
-     * Test logic for the actual completion date field on the finding model
-     */
-    public function testAcd($value='')
-    {
-        $finding = new Finding();
-
-        // The ACD is initially null
-        $this->assertNull($finding->actualCompletionDate);
-
-        // When the finding enters EA, the ACD should be set to today's date
-        $finding->status = 'EA';
-
-        $today = Zend_Date::now();
-        $acd = new Zend_Date();
-        $acd->set($finding->actualCompletionDate, Zend_Date::ISO_8601);
-
-        $this->assertEquals($today->get(Zend_Date::DATE_SHORT), $acd->get(Zend_Date::DATE_SHORT));
-
-        // When a finding goes back to EN status, the ACD should be null again
-        $finding->status = 'EN';
-        $this->assertNull($finding->actualCompletionDate);
     }
 
     /**
