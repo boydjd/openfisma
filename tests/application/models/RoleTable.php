02006b05f7a1d6c81a110c2dc06ae31651738c70
Duy K. Bui <duy.bui@endeavorsystems.com>
2013-03-25 09:51:45 -0400

    Admin: Fix UnitTest

--- tests/application/models/RoleTable.php
+++ tests/application/models/RoleTable.php
@@ -4,53 +4,53 @@
  *
  * This file is part of OpenFISMA.
  *
- * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see
  * {@link http://www.gnu.org/licenses/}.
  */
 
 require_once(realpath(dirname(__FILE__) . '/../../Case/Unit.php'));
 
 /**
- * Test_Application_Models_RoleTable 
- * 
+ * Test_Application_Models_RoleTable
+ *
  * @uses Test_Case_Unit
- * @package Test 
+ * @package Test
  * @copyright (c) Endeavor Systems, Inc. 2011 {@link http://www.endeavorsystems.com}
- * @author Josh Boyd <joshua.boyd@endeavorsystems.com> 
+ * @author Josh Boyd <joshua.boyd@endeavorsystems.com>
  * @license http://www.openfisma.org/content/license GPLv3
  */
 class Test_Application_Models_RoleTable extends Test_Case_Unit
 {
     /**
-     * Check if getSearchableFields() returns a not-empty array 
-     * 
+     * Check if getSearchableFields() returns a not-empty array
+     *
      * @access public
      * @return void
      */
     public function testGetSearchableFields()
     {
-        $searchableFields = RoleTable::getSearchableFields();
+        $searchableFields = Doctrine::getTable('Role')->getSearchableFields();
 
         $this->assertTrue(is_array($searchableFields));
         $this->assertNotEmpty($searchableFields);
     }
 
     /**
-     * testGetAclFields 
-     * 
+     * testGetAclFields
+     *
      * @access public
      * @return void
      */
     public function testGetAclFields()
     {
-        $this->assertTrue(is_array(RoleTable::getAclFields()));
+        $this->assertTrue(is_array(Doctrine::getTable('Role')->getAclFields()));
     }
 }
