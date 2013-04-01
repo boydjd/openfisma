692e1692a7b8a40cfeaf25a4a3cd46533e215260
Duy K. Bui <duy.bui@endeavorsystems.com>
2013-04-01 16:10:23 -0400

    OFJ-1961 AppDetective Plugin

--- library/Fisma/Inject/AppDetective.php
+++ library/Fisma/Inject/AppDetective.php
@@ -4,15 +4,15 @@
  *
  * This file is part of OpenFISMA.
  *
- * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+ * OpenFISMA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * OpenFISMA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see 
+ * You should have received a copy of the GNU General Public License along with OpenFISMA.  If not, see
  * {@link http://www.gnu.org/licenses/}.
  */
 
@@ -21,13 +21,13 @@
  *
  * This plug-in makes heavy use of the SimpleXML xpath() function, which makes code easier to maintain, but could also
  * be a performance bottleneck for large reports. Currently there has not been any load-testing for this plugin.
- * 
+ *
  * @author     Mark E. Haase <mhaase@endeavorsystems.com>
  * @copyright  (c) Endeavor Systems, Inc. 2009 {@link http://www.endeavorsystems.com}
  * @license    http://www.openfisma.org/content/license GPLv3
  * @package    Fisma
  * @subpackage Fisma_Inject
- * 
+ *
  * @todo       Add audit logging
  */
 class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
@@ -38,16 +38,16 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
      * amount will be truncated and a warning will be issued.
      */
     const MAX_VULN_DETAILS_PER_FINDING = 25;
-    
+
     /**
      * This is marketing language which is filtered out of the recommendation field.
      */
     const REMOVE_PHRASE = "/As part of a complete .* level of database security./";
-    
+
     /**
      * Implements the required function in the Inject_Abstract interface. This parses the report and commits
      * all data to the database.
-     * 
+     *
      * @param string $uploadId The specified id of upload file to be parsed
      * @return void
      * @throws Fisma_Zend_Exception_InvalidFileFormat if the file is not an App Detective report
@@ -56,9 +56,9 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
     {
         // Parse the XML file
         $report  = new XMLReader();
-        
-        // Bug 2596247 - "App Detective plug-in does not work with recent vrsn. of AD"        
-        // Make sure that this is an AppDetective report, not a Crystal report. (App Detective can generate both 
+
+        // Bug 2596247 - "App Detective plug-in does not work with recent vrsn. of AD"
+        // Make sure that this is an AppDetective report, not a Crystal report. (App Detective can generate both
         // kinds of report.)
         if ($report->lookupNamespace('urn:crystal-reports:schemas')) {
             throw new Fisma_Zend_Exception_InvalidFileFormat('This is a Crystal Report, not an App Detective report.');
@@ -77,7 +77,7 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
 
         $report->close();
     }
-    
+
     /**
      * Get and save findings, assets and products info are recorded in the report.
      *
@@ -96,28 +96,28 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
         while ($oXml->read()) {
 
             // The elements of the XML that we care about don't occur until we reach a depth of 2
-            if ($oXml->depth >= 2 && $oXml->nodeType == XMLReader::ELEMENT) {
+            if ($oXml->depth >= 1 && $oXml->nodeType == XMLReader::ELEMENT) {
                 if ($oXml->name == 'testDate') {
-          
+
                     // Parse the discovered date/time out of the testDate field
                     $testDateString = $oXml->readString();
                     $testDate = array();
-                    if (!preg_match('/\d{1,2}\/\d{1,2}\/\d{4} \d{1,2}:\d{1,2}:\d{1,2} [AP]M/', 
-                                    $testDateString, 
+                    if (!preg_match('/\d{1,2}\/\d{1,2}\/\d{4} \d{1,2}:\d{1,2}:\d{1,2} [AP]M/',
+                                    $testDateString,
                                     $testDate)) {
                         throw new Fisma_Zend_Exception_InvalidFileFormat(
                             "Unable to parse the date from the testDate field: \"$testDateString\""
                         );
                     }
-                   
+
                     $discoveredDate = new Zend_Date($testDate[0]);
                 } elseif ($oXml->name == 'appName') {
-                    
+
                     // Asset information is parsed out of the appName field.
                     // There should only be 1 appName field in the entire report.
                     $reportAppName = $oXml->readString();
                     $appName = array();
-                                        
+
                     if (preg_match('/\((.*?)\)/', $reportAppName, $appName)) {
 
                     // If a parenthesized expression is found, then use the parenthesized expression.
@@ -137,14 +137,14 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
                     }
 
                     $asset['addressIp'] = $ipAddress[0]; // the regex only has one match by its definition
-        
+
                     // Parse out port number
                     $port = array();
                     if (!preg_match('/\bport (\d{1,5})\b/i', $reportAppName, $port)) {
                         throw new Fisma_Zend_Exception_InvalidFileFormat(
                             "Unable to parse the port number from the appName field: \"$reportAppName\""
                         );
-                    }  
+                    }
 
                     $asset['addressPort'] = $port[1]; // match the parenthesized part of the regex
                     $asset['source'] = 'scan';
@@ -156,8 +156,8 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
                     try {
 
                         // Bug 2596247 - "App Detective plug-in does not work with recent vrsn. of AD"
-                        // App Detective does not follow the CPE specification when it cannot identify the platform. 
-                        // It creates a CPE called "cpe:no-match", which is not valid and will cause the Cpe class 
+                        // App Detective does not follow the CPE specification when it cannot identify the platform.
+                        // It creates a CPE called "cpe:no-match", which is not valid and will cause the Cpe class
                         // to throw an exception.
                         $cpe = new Fisma_Cpe($oXml->getAttribute('name'));
                         $product['cpeName'] = $cpe->cpeName;
@@ -175,10 +175,10 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
                     $findings[$itemCounter]['recommendation'] = $oXml->readString();
                 } elseif ($oXml->name == 'details') {
                     $findings[$itemCounter]['findingDetail'][$detailCounter] = $oXml->readString();
-                    
-                } 
+
+                }
             } elseif ($oXml->nodeType == XMLReader::END_ELEMENT) {
-                if ($oXml->name == 'data') {
+                if ($oXml->name == 'data' || $oXml->name == 'vulnerability') {
                     $itemCounter++;
                     $detailCounter = 0;
                 } elseif ($oXml->name =='details') {
@@ -186,8 +186,8 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
                 }
             }
         }
-    
-        $this->_saveData($uploadId, $findings, $asset, $product); 
+
+        $this->_saveData($uploadId, $findings, $asset, $product);
     }
 
     /**
@@ -201,9 +201,9 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
     private function _saveData($uploadId, $findings, $asset, $product)
     {
         foreach ($findings as $finding) {
-        
-            // Some "findings" are empty or other levels such as Informational. We test for emptiness by 
-            // looking at the risk element -- something which all the findings should have. If a finding is missing 
+
+            // Some "findings" are empty or other levels such as Informational. We test for emptiness by
+            // looking at the risk element -- something which all the findings should have. If a finding is missing
             // a risk element or its level is not HIGH or MEDIUM or LOW, then we silently skip it.
             $threatLevel = strtoupper($finding['risk']);
             if (!empty($threatLevel) && ($threatLevel == 'HIGH' || $threatLevel == 'MEDIUM' || $threatLevel == 'LOW')) {
@@ -220,13 +220,13 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
                 //todo english translate "medium" into "MODERATE" to adapt OpenFISMA
                 if ('MEDIUM' == $threatLevel) {
                     $findingInstance['threatLevel'] = 'MODERATE';
-                }   
+                }
 
-                $findingInstance['threat'] = (!empty($finding['threat'])) ? 
+                $findingInstance['threat'] = (!empty($finding['threat'])) ?
                                              Fisma_String::textToHtml($finding['threat']) : NULL;
                 if (!empty($finding['recommendation'])) {
-                    $findingInstance['recommendation'] = preg_replace(self::REMOVE_PHRASE, 
-                                                                      '', 
+                    $findingInstance['recommendation'] = preg_replace(self::REMOVE_PHRASE,
+                                                                      '',
                                                                       $finding['recommendation']);
                     $findingInstance['recommendation'] = Fisma_String::textToHtml($findingInstance['recommendation']);
                 } else {
@@ -250,7 +250,7 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
                                          . ' truncated when this finding was injected due to storage constraints.</i>';
                             break;
                         }
-                    }        
+                    }
                     $findingData .= '</ul>';
                 }
                 $findingInstance['description'] =  Fisma_String::textToHtml($findingData);
@@ -260,5 +260,5 @@ class Fisma_Inject_AppDetective extends Fisma_Inject_Abstract
             }
         }
         $this->_commit();
-    } 
+    }
 }
