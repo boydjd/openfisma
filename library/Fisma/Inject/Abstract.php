062f8733c50c05b50092c3fbb0cbf935e5299f94
Andrew Reeves <andrew.reeves@endeavorsystems.com>
2013-04-23 13:42:48 -0400

    OFJ-2209 asset injection malfunctioning
    Gave up on any hope of making the Fisma_Inject_Abstract *correct* and just cludged it.

--- library/Fisma/Inject/Abstract.php
+++ library/Fisma/Inject/Abstract.php
@@ -440,8 +440,13 @@ abstract class Fisma_Inject_Abstract
         // Check to see if any of the pending assets are duplicates, if so, update the finding to point to the correct
         // asset id
         foreach ($this->_findings as &$findingData) {
-            if (empty($findingData['finding']->Asset) && $findingData['asset'] == $assetData) {
-                $findingData['asset']['id'] = $id;
+            if (empty($findingData['finding']->Asset)) {
+                if (
+                    $findingData['asset']['networkId'] === $asset->networkId
+                    && $findingData['asset']['addressIp'] === $asset->addressIp
+                ) {
+                    $findingData['asset']['id'] = $id;
+                }
             }
         }
         // Free object
