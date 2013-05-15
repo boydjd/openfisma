6614b224b15fbb2937dd2d76c2888296f7f5a573
Yusef Pogue <yusef.pogue@endeavorsystems.com>
2013-05-15 16:09:06 -0400

    OFJ-2251 - IRTM User Not Reported Correctly (ZD#2334) check to make sure $incident exist

--- application/modules/default/controllers/IncidentController.php
+++ application/modules/default/controllers/IncidentController.php
@@ -566,8 +566,11 @@ class IncidentController extends Fisma_Zend_Controller_Action_Object
         $incident = unserialize($session->irDraft);
         $incident->delete();
         
-        if (isset($session->irDraft)) {
+        if ( isset($incident) ) {
             unset($incident);
+        }
+        
+        if (isset($session->irDraft)) {
             unset($session->irDraft);
         }
     }
